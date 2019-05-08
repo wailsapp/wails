319dbcdc49319817e0c0bae7a0d6a9110c032875
Lea Anthony <lea.anthony@gmail.com>
2019-05-08 22:42:13 +1000

    chore: lint fix

--- cmd/windows.go
+++ cmd/windows.go
@@ -14,6 +14,6 @@ func init() {
 	stdout := windows.Handle(os.Stdout.Fd())
 	var originalMode uint32
 
-	windows.GetConsoleMode(stdout, &originalMode)
-	windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	_ = windows.GetConsoleMode(stdout, &originalMode)
+	_ = windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 }
