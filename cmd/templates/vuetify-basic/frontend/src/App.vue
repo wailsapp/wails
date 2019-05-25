6b1b99954653a34ab20a13028537ea558eaffa21
Lea Anthony <lea.anthony@gmail.com>
2019-05-25 11:13:34 +1000

    feat: add vuetify template

---
+++ cmd/templates/vuetify-basic/frontend/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid class="px-0">
+        <v-layout justify-center align-center class="px-0">
+          <hello-world></hello-world>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span style="margin-left:1em">&copy; You</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  data: () => ({
+    drawer: false
+  }),
+  components: {
+    HelloWorld
+  },
+  props: {
+    source: String
+  }
+};
+</script>
+
+<style>
+.logo {
+  width: 16em;
+}
+</style>
