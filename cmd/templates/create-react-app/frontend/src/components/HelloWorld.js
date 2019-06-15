393e418e6819960936b84de6954b4692dd35b8b9
ktc <ktc@protonmail.com>
2019-06-15 14:52:47 +0300

    react template

---
+++ cmd/templates/create-react-app/frontend/src/components/HelloWorld.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+class HelloWorld extends React.Component {
+  constructor(props, context) {
+    super();
+    this.state = {
+      showModal: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+
+    window.backend.basic().then(result =>
+      this.setState({
+        result
+      })
+    );
+  }
+
+  handleCloseModal () {
+    this.setState({ showModal: false });
+  }
+
+
+  startAsync() {
+    this.setState({
+      loading: true
+    });
+
+    window.backend.basic().then(result =>
+      this.setState({
+        loading: false,
+        result
+      })
+    );
+  }
+
+  render() {
+    const { loading, result } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.handleOpenModal} type="button">
+          Hello
+        </button>
+        <Modal
+          isOpen={this.state.showModal}
+          contentLabel="Minimal Modal Example"
+        >
+        <p>{result}</p>
+        <button onClick={this.handleCloseModal}>Close Modal</button>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
