52afbd3f154c30746784b7440a1a4e1acd3fd702
Lea Anthony <lea.anthony@gmail.com>
2019-07-01 09:01:07 +1000

    fix: revert back to my-app

--- cmd/templates/angular-template/frontend/src/app/app.component.ts
+++ cmd/templates/angular-template/frontend/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '{{.Name}}';
+  title = 'my-app';
 
   clickMessage = '';
 
