386ec5e46f89580cfbee6dcfdcba0c4b470083b3
bh90210 <ktc@protonmail.com>
2019-08-30 16:01:51 +0300

    feat: kali linux support

--- cmd/system.go
+++ cmd/system.go
@@ -274,7 +274,7 @@ func CheckDependencies(logger *Logger) (bool, error) {
 		distroInfo := GetLinuxDistroInfo()
 
 		switch distroInfo.Distribution {
-		case Ubuntu, Debian, Zorin, Parrot, Linuxmint, Elementary:
+		case Ubuntu, Debian, Zorin, Parrot, Linuxmint, Elementary, Kali:
 			libraryChecker = DpkgInstalled
 		case Arch:
 			libraryChecker = PacmanInstalled
