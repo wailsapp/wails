694f80434a2346123eb0ed92cc53fc076c3ffe0a
Lea Anthony <lea.anthony@gmail.com>
2019-09-05 08:50:40 +1000

    fix: revert fix Masterminds have fixed it upstream

--- cmd/semver.go
+++ cmd/semver.go
@@ -8,7 +8,7 @@ import (
 
 // SemanticVersion is a struct containing a semantic version
 type SemanticVersion struct {
-	Version semver.Version
+	Version *semver.Version
 }
 
 // NewSemanticVersion creates a new SemanticVersion object with the given version string
