702b475a3b216714fe4284eccd7cc3eda78918ef
Michael Hipp <michael@redmule.com>
2019-09-06 05:54:28 -0500

    Initial create of vuetify2-basic folder

---
+++ cmd/templates/vuetify2-basic/frontend/src/main.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill';
+import Vue from 'vue';
+
+// Setup Vuetify
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont';
+
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+import Wails from '@wailsapp/runtime';
+
+Wails.Init(() => {
+	new Vue({
+		render: h => h(App)
+	}).$mount('#app');
+});
