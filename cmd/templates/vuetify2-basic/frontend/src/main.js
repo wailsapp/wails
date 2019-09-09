245f678aa1b10bafb5166a7d9888340f66e04d71
Michael Hipp <michael@redmule.com>
2019-09-08 19:41:00 -0500

    Remove babel-polyfill, add mdi/font

--- cmd/templates/vuetify2-basic/frontend/src/main.js
+++ cmd/templates/vuetify2-basic/frontend/src/main.js
@@ -1,20 +1,27 @@
-import 'babel-polyfill';
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
-import App from './App.vue';
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-Vue.config.devtools = true;
+Vue.config.productionTip = false
+Vue.config.devtools = true
 
-import Wails from '@wailsapp/runtime';
+import Wails from '@wailsapp/runtime'
 
 Wails.Init(() => {
-	new Vue({
-		vuetify: new Vuetify({}),
-		render: h => h(App)
-	}).$mount('#app')
-});
+  new Vue({
+    vuetify: new Vuetify({
+      icons: {
+        iconfont: 'mdi'
+      },
+      theme: {
+        dark: true
+      }
+    }),
+    render: h => h(App)
+  }).$mount('#app')
+})
