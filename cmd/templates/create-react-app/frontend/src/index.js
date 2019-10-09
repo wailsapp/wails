d399b7580d20f5c48815017c26e1fbca8d90e92a
Lea Anthony <lea.anthony@gmail.com>
2019-10-09 19:34:24 +1100

    Develop (#255)
    Hotfix

--- cmd/templates/create-react-app/frontend/src/index.js
+++ cmd/templates/create-react-app/frontend/src/index.js
@@ -4,7 +4,7 @@ import 'core-js/stable';
 import './index.css';
 import App from './App';
 
-import Wails from '@wailsapp/runtime';
+import * as Wails from '@wailsapp/runtime';
 
 Wails.Init(() => {
   ReactDOM.render(<App />, document.getElementById('app'));
