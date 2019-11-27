288feee1d9fe6458afeebdd317a8bf975f6298e9
Lea Anthony <lea.anthony@gmail.com>
2019-11-27 22:30:27 +1100

    angular windows fixes

--- app.go
+++ app.go
@@ -80,7 +80,7 @@ func CreateApp(optionalConfig ...*AppConfig) *App {
 
 // SetProcessDPIAware via user32.dll
 // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setprocessdpiaware
-// Also, thanks Jack Mordaynt! https://github.com/wailsapp/wails/issues/293
+// Also, thanks Jack Mordaunt! https://github.com/wailsapp/wails/issues/293
 func SetProcessDPIAware() error {
 	status, r, err := syscall.NewLazyDLL("user32.dll").NewProc("SetProcessDPIAware").Call()
 	if status == 0 {
