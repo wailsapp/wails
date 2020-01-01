7352e9d6a282281b4690416f466e9c0b6020eef2
Travis McLane <tmclane@gmail.com>
2020-01-01 14:20:40 -0600

    fix: codacy corrections

--- cmd/templates/vuetify2-basic/frontend/babel.config.js
+++ cmd/templates/vuetify2-basic/frontend/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
-}
+	presets: [
+		'@vue/app'
+	]
+};
--- cmd/templates/vuetify2-basic/frontend/src/main.js
+++ cmd/templates/vuetify2-basic/frontend/src/main.js
@@ -1,27 +1,27 @@
-import '@mdi/font/css/materialdesignicons.css'
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
-import App from './App.vue'
+import App from './App.vue';
 
-Vue.config.productionTip = false
-Vue.config.devtools = true
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
 
-import Wails from '@wailsapp/runtime'
+import Wails from '@wailsapp/runtime';
 
 Wails.Init(() => {
-  new Vue({
-    vuetify: new Vuetify({
-      icons: {
-        iconfont: 'mdi'
-      },
-      theme: {
-        dark: true
-      }
-    }),
-    render: h => h(App)
-  }).$mount('#app')
-})
+	new Vue({
+		vuetify: new Vuetify({
+			icons: {
+				iconfont: 'mdi'
+			},
+			theme: {
+				dark: true
+			}
+		}),
+		render: h => h(App)
+	}).$mount('#app');
+});
