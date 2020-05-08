6339f230f9450f83df89ab3f3f6f04f7662d8bd4
Lea Anthony <lea.anthony@gmail.com>
2020-05-09 06:04:55 +1000

    Fix CWE-126

--- lib/renderer/webview/webview.h
+++ lib/renderer/webview/webview.h
@@ -1939,6 +1939,7 @@ struct webview_priv
 
     webview_dialog(w, WEBVIEW_DIALOG_TYPE_OPEN, WEBVIEW_DIALOG_FLAG_FILE, "", "",
                    filename, 255);
+    filename[255] = '\0';
     if (strlen(filename) > 0)
     {
       [listener chooseFilename:[NSString stringWithUTF8String:filename]];
