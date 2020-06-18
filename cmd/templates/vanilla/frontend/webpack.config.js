bce686d77946f3548972965eeaa15ca5c30d2ef2
Lea Anthony <lea.anthony@gmail.com>
2020-06-18 20:30:31 +1000

    lint fixes

--- cmd/templates/vanilla/frontend/webpack.config.js
+++ cmd/templates/vanilla/frontend/webpack.config.js
@@ -1,13 +1,13 @@
-const path = require("path");
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 let imageSizeLimit = 9999999999999999;
-let sourceDir = path.resolve(__dirname, "src");
-let buildDir = path.resolve(__dirname, "build");
+let sourceDir = path.resolve(__dirname, 'src');
+let buildDir = path.resolve(__dirname, 'build');
 
 module.exports = {
 	entry: {
-		index: path.resolve(sourceDir, "main.js")
+		index: path.resolve(sourceDir, 'main.js')
 	},
 	output: {
 		path: buildDir,
@@ -30,7 +30,7 @@ module.exports = {
 				test: /\.(png|gif|jpg|woff2?|eot|ttf|otf|svg)(\?.*)?$/i,
 				use: [
 					{
-						loader: "url-loader",
+						loader: 'url-loader',
 						options: {
 							limit: imageSizeLimit
 						}
@@ -43,12 +43,12 @@ module.exports = {
         new CopyWebpackPlugin({
 			patterns: [
 				{ 
-					from: path.resolve(sourceDir, "main.css"), 
-					to: path.resolve(buildDir, "main.css") 
+					from: path.resolve(sourceDir, 'main.css'), 
+					to: path.resolve(buildDir, 'main.css') 
 				},
 				{ 
-					from: path.resolve(sourceDir, "index.html"), 
-					to: path.resolve(buildDir, "index.html") 
+					from: path.resolve(sourceDir, 'index.html'), 
+					to: path.resolve(buildDir, 'index.html') 
 				},
 				
 			]
