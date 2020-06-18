bbc16fe03a372fec721685e94bb001e26b15067b
Lea Anthony <lea.anthony@gmail.com>
2020-06-19 09:38:01 +1000

    Lint fixes

--- cmd/templates/vanilla/frontend/src/main.js
+++ cmd/templates/vanilla/frontend/src/main.js
@@ -5,25 +5,24 @@ const runtime = require('@wailsapp/runtime');
 // running our JS
 runtime.Init(() => {
 
-    // Ensure the default app div is 100% wide/high
-    var app = document.getElementById("app");
-    app.style.width = "100%";
-    app.style.height = "100%";
+	// Ensure the default app div is 100% wide/high
+	var app = document.getElementById('app');
+	app.style.width = '100%';
+	app.style.height = '100%';
 
-    // Inject html
-    app.innerHTML = `
-    <div class="logo"></div>
-    <div class="container">
-        <button id="button">Click Me!</button>
-        <div id="result"/>
-    </div>
-    `;
+	// Inject html
+	app.innerHTML = `
+	<div class='logo'></div>
+	<div class='container'>
+			<button id='button'>Click Me!</button>
+			<div id='result'/>
+	</div>
+	`;
 
-    // Connect button to Go method
-    document.getElementById("button").onclick = () => {
-       window.backend.basic().then((result) => {
-           document.getElementById("result").innerText = result;
-       })
-    } 
-
+	// Connect button to Go method
+	document.getElementById('button').onclick = () => {
+		window.backend.basic().then((result) => {
+			document.getElementById('result').innerText = result;
+		})
+	} 
 });
--- cmd/templates/vanilla/frontend/webpack.config.js
+++ cmd/templates/vanilla/frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-let imageSizeLimit = 9999999999999999;
+let imageSizeLimit = 9007199254740991; // Number.MAX_SAFE_INTEGER
 let sourceDir = path.resolve(__dirname, 'src');
 let buildDir = path.resolve(__dirname, 'build');
 
@@ -40,19 +40,17 @@ module.exports = {
 		]
 	},
 	plugins: [
-        new CopyWebpackPlugin({
+		new CopyWebpackPlugin({
 			patterns: [
-				{ 
-					from: path.resolve(sourceDir, 'main.css'), 
-					to: path.resolve(buildDir, 'main.css') 
+				{
+					from: path.resolve(sourceDir, 'main.css'),
+					to: path.resolve(buildDir, 'main.css')
 				},
-				{ 
-					from: path.resolve(sourceDir, 'index.html'), 
-					to: path.resolve(buildDir, 'index.html') 
+				{
+					from: path.resolve(sourceDir, 'index.html'),
+					to: path.resolve(buildDir, 'index.html')
 				},
-				
 			]
 		})
-    ]
-}
-
+	]
+};
