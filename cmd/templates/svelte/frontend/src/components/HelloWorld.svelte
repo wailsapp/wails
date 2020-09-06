0bae54ef8a02e184fa10574eaff3700d4437f46f
Tim Kipp <timkipp.22.developer@gmail.com>
2020-09-06 16:00:51 -0700

    feat: added Svelte template

---
+++ cmd/templates/svelte/frontend/src/components/HelloWorld.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { getContext } from 'svelte';
+	import ModalContent from './ModalContent.svelte'
+	
+	const { open } = getContext('simple-modal');
+
+	const handleOpenModal = () => {
+		window.backend.basic().then((result) => {
+			open(ModalContent, { message: result });
+		});
+	};
+</script>
+
+<main>
+	<p><button on:click={handleOpenModal}>Hello</button></p>
+</main>
+
+<style></style>
