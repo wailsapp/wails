31494bba22f69ab7a2ef4b8afebe26d7747b9ff0
Travis McLane <tmclane@gmail.com>
2020-09-11 17:41:42 -0500

    add darwin platform for server

---
+++ v2/internal/runtime/js/server/darwin.js
@@ -0,0 +1,21 @@
+/*
+ _	   __	  _ __
+| |	 / /___ _(_) /____
+| | /| / / __ `/ / / ___/
+| |/ |/ / /_/ / / (__  )
+|__/|__/\__,_/_/_/____/
+The lightweight framework for web-like apps
+(c) Lea Anthony 2019-present
+*/
+/* jshint esversion: 6 */
+
+/**
+ * Initialises platform specific code
+ */
+
+export const System = {
+    Platform: "darwin",
+    AppType: "server"
+}
+
+export function Init() { }
