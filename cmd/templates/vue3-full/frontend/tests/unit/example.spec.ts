9d8efd4a453d23d1ce1f0789371e983fe2522aef
Kyle Muchmore <kyle.muchmore@kickview.com>
2020-10-08 20:53:19 -0600

    feat: Vue3, Vue Router, Vuex, and Typescript Template

---
+++ cmd/templates/vue3-full/frontend/tests/unit/example.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    expect(wrapper.text()).to.include(msg)
+  })
+})
