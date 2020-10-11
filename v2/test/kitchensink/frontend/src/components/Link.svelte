39c599d2dedb7e5c7f3130aca4b290973691edd2
Lea Anthony <lea.anthony@gmail.com>
2020-10-12 06:56:32 +1100

    Link component

---
+++ v2/test/kitchensink/frontend/src/components/Link.svelte
@@ -0,0 +1,37 @@
+<script>
+    import runtime from '@wails/runtime';
+    import { darkMode } from '../Store';
+
+    export let href;
+
+    $: linkclass = darkMode ? 'dark' : 'light';
+
+    function openLink() {
+        runtime.Browser.OpenURL(href);
+    }
+</script>
+
+<span class="link {linkclass}" on:click="{openLink}"><slot></slot></span>
+
+<style>
+
+    .link {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+
+    .link.dark:hover {
+        color: #ff8bfb;
+    }
+
+    .link.dark {
+        color: #ff8bfb88;
+    }
+
+    .link.light {
+        color: #5c5cff;
+        text-decoration: underline;
+    }
+
+    
+</style>
