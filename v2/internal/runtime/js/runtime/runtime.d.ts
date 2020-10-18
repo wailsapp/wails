78f99c2697c28f0625efe297173ddb46bb10486e
Lea Anthony <lea.anthony@gmail.com>
2020-10-18 11:48:56 +1100

    update runtime definitions

--- v2/internal/runtime/js/runtime/runtime.d.ts
+++ v2/internal/runtime/js/runtime/runtime.d.ts
@@ -22,13 +22,13 @@ declare const wailsapp__runtime: {
     };
     Events: {
         Acknowledge(eventName: string): void; 
-        Emit(eventName: string): void;
+        Emit(eventName: string, data?: any): void;
         Heartbeat(eventName: string, timeInMilliseconds: number, callback: () => void): void;
-        On(eventName: string, callback: () => void): void;
-        OnMultiple(eventName: string, callback: () => void, maxCallbacks: number): void;
-        Once(eventName: string, callback: () => void): void;
+        On(eventName: string, callback: (data?: any) => void): void;
+        OnMultiple(eventName: string, callback: (data?: any) => void, maxCallbacks: number): void;
+        Once(eventName: string, callback: (data?: any) => void): void;
     };
-    Init(callback: () => void): void;
+    // Init(callback: () => void): void;
     Log: {
         Debug(message: string): void;
         Error(message: string): void;
