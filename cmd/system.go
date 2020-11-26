7e67562e19dbcbc29d554164ee31f1abe77c5b88
Lea Anthony <lea.anthony@gmail.com>
2020-11-26 20:44:42 +1100

    Support EndeavourOS (#565)

--- cmd/system.go
+++ cmd/system.go
@@ -276,7 +276,7 @@ func CheckDependencies(logger *Logger) (bool, error) {
 		switch distroInfo.Distribution {
 		case Ubuntu, Debian, Zorin, Parrot, Linuxmint, Elementary, Kali, Neon, Deepin, Raspbian, PopOS:
 			libraryChecker = DpkgInstalled
-		case Arch, ArcoLinux, ArchLabs, Ctlos, Manjaro, ManjaroARM:
+		case Arch, ArcoLinux, ArchLabs, Ctlos, Manjaro, ManjaroARM, EndeavourOS:
 			libraryChecker = PacmanInstalled
 		case CentOS, Fedora, Tumbleweed, Leap:
 			libraryChecker = RpmInstalled
