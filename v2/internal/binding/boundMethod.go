7573f68df3b71d3c5e73b566b405c798cecc7fb7
Lea Anthony <lea.anthony@gmail.com>
2021-01-31 15:35:33 +1100

    Add argument guard for methods

--- v2/internal/binding/boundMethod.go
+++ v2/internal/binding/boundMethod.go
@@ -30,6 +30,9 @@ func (b *BoundMethod) OutputCount() int {
 func (b *BoundMethod) ParseArgs(args []json.RawMessage) ([]interface{}, error) {
 
 	result := make([]interface{}, b.InputCount())
+	if len(args) != b.InputCount() {
+		return nil, fmt.Errorf("received %d arguments to method '%s', expected %d", len(args), b.Name, b.InputCount())
+	}
 	for index, arg := range args {
 		typ := b.Inputs[index].reflectType
 		inputValue := reflect.New(typ).Interface()
