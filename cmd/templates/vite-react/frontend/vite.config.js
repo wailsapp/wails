204c15f1681bbca10f33e2c9ab3f0c1d316da165
Thomas B Homburg <thomas@madethis.dk>
2021-03-24 22:05:25 +0100

    More prettier

--- cmd/templates/vite-react/frontend/vite.config.js
+++ cmd/templates/vite-react/frontend/vite.config.js
@@ -1,13 +1,13 @@
-import path from "path";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import path from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default {
-  plugins: [reactRefresh()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/main.tsx"),
-      formats: ["es"],
-      name: "main",
-    },
-  },
+	plugins: [reactRefresh()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/main.tsx'),
+			formats: ['es'],
+			name: 'main',
+		},
+	},
 };
