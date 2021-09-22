23d53253f9eee149077d5c1597e2f2be9b2f762d
misitebao <i@misitebao.com>
2021-09-23 02:11:12 +0800

    [v2]feat: optimize the template content of the template command

--- v2/cmd/wails/internal/commands/generate/template/base/main.tmpl.go
+++ v2/cmd/wails/internal/commands/generate/template/base/main.tmpl.go
@@ -5,28 +5,49 @@ import (
 	"log"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed frontend/dist
 var assets embed.FS
 
 func main() {
-
-	// Create application with options
+	// Create an instance of the app structure
 	app := NewApp()
 
+	// Create application with options
 	err := wails.Run(&options.App{
-		Title:      "{{.ProjectName}}",
-		Width:      1024,
-		Height:     768,
-		Assets:     assets,
+		Title:  "{{.ProjectName}}",
+		Width:  1024,
+		Height: 768,
+		// MinWidth:          720,
+		// MinHeight:         570,
+		// MaxWidth:          1280,
+		// MaxHeight:         740,
+		DisableResize:     false,
+		Fullscreen:        false,
+		Frameless:         false,
+		StartHidden:       false,
+		HideWindowOnClose: false,
+		RGBA:              &options.RGBA{255, 255, 255, 255},
+		Assets:            assets,
+		LogLevel:   logger.DEBUG,
 		OnStartup:  app.startup,
+		OnDomReady: app.domReady,
 		OnShutdown: app.shutdown,
 		Bind: []interface{}{
 			app,
 		},
+		// Windows platform specific options
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			DisableWindowIcon:    false,
+		},
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
