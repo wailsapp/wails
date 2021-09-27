589eb3864fcc99e49382e8aedcd9fcd51f0e1bcc
Lea Anthony <lea.anthony@gmail.com>
2021-09-27 19:35:30 +1000

    [v2] Docs update

---
+++ website/src/pages/showcase/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+    return (
+        <Layout>
+            <h1>My React page</h1>
+            <p>This is a React page</p>
+        </Layout>
+    );
+}
