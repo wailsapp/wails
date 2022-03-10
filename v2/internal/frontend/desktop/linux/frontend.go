f71b53dddff66d769fe6929ba62491033a79d54d
Florent Heyworth <florent@heyworth.tech>
2022-03-10 09:59:29 +0100

    ensure binary resources can be served fixes #1235

--- v2/internal/frontend/desktop/linux/frontend.go
+++ v2/internal/frontend/desktop/linux/frontend.go
@@ -306,13 +306,19 @@ func (f *Frontend) processRequest(request unsafe.Pointer) {
 		return
 	}
 
-	cContent := C.CString(string(res.Body))
-	defer C.free(unsafe.Pointer(cContent))
+	var cContent unsafe.Pointer
+	bodyLen := len(res.Body)
+	if bodyLen > 0 {
+		cContent = C.malloc(C.ulong(bodyLen))
+		C.memcpy(cContent, unsafe.Pointer(&res.Body[0]), C.size_t(bodyLen))
+	}
+
 	cMimeType := C.CString(res.MimeType)
 	defer C.free(unsafe.Pointer(cMimeType))
-	cLen := C.long(C.strlen(cContent))
+
+	cLen := C.long(len(res.Body))
 	stream := C.g_memory_input_stream_new_from_data(
-		unsafe.Pointer(C.g_strdup(cContent)),
+		cContent,
 		cLen,
 		(*[0]byte)(C.g_free))
 	C.webkit_uri_scheme_request_finish(req, stream, cLen, cMimeType)
