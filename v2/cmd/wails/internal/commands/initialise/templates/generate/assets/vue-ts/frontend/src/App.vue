fac2a8ba3cc28b1f2c6c86cf95134c61c292f92e
Lea Anthony <lea.anthony@gmail.com>
2022-05-01 10:41:07 +1000

    Closes #1357 #1366

--- v2/cmd/wails/internal/commands/initialise/templates/generate/assets/vue-ts/frontend/src/App.vue
+++ v2/cmd/wails/internal/commands/initialise/templates/generate/assets/vue-ts/frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'</script>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <template>
   <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
