912d7cf34b68fe98bacb99140e329df27736cd7f
Lea Anthony <lea.anthony@gmail.com>
2022-05-01 10:18:38 +1000

    Add startup method to templates by default

--- v2/cmd/wails/internal/commands/initialise/templates/templates/vue-ts/app.tmpl.go
+++ v2/cmd/wails/internal/commands/initialise/templates/templates/vue-ts/app.tmpl.go
@@ -15,6 +15,12 @@ func NewApp() *App {
 	return &App{}
 }
 
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
