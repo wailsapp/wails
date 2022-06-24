58a59fba984baab8c2408c86d1005ce5499fa373
tomanagle <tomanagle@gmail.com>
2022-06-24 17:25:13 +1000

    fix: remove semi

--- v2/cmd/wails/internal/commands/initialise/templates/generate/assets/react-ts/frontend/src/main.tsx
+++ v2/cmd/wails/internal/commands/initialise/templates/generate/assets/react-ts/frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './style.css';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './style.css'
+import App from './App'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 
-const root = createRoot(container!);
+const root = createRoot(container!)
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
