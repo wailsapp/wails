62e1a7d15838a271cf6109aa98411d12be49c79f
tomanagle <tomanagle@gmail.com>
2022-06-24 14:14:57 +1000

    fix: quotes

--- v2/cmd/wails/internal/commands/initialise/templates/generate/assets/react-ts/frontend/src/main.tsx
+++ v2/cmd/wails/internal/commands/initialise/templates/generate/assets/react-ts/frontend/src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./style.css";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import App from './App';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 
 const root = createRoot(container!);
 
