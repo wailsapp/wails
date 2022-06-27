d9888ce6af89c7b5f22623b4db266efeae26df32
Tom <tomanagle@gmail.com>
2022-06-27 18:00:04 +1000

    Chore/react 18 create root (#1489)
    * chore: use createRoot for react-ts
    
    * chore: use createRoot for react-js
    
    * fix: quotes
    
    * fix: remove semi
    
    * chore: update react templates to use createRoot instead of ReactDOM.render
    
    Co-authored-by: Lea Anthony <lea.anthony@gmail.com>

--- v2/cmd/wails/internal/commands/initialise/templates/generate/assets/preact/frontend/src/main.jsx
+++ v2/cmd/wails/internal/commands/initialise/templates/generate/assets/preact/frontend/src/main.jsx
@@ -1,5 +1,14 @@
-import {render} from 'preact';
-import {App} from './app';
-import './style.css';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './style.css'
+import App from './App'
 
-render(<App/>, document.getElementById('app'));
+const container = document.getElementById('root')
+
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
