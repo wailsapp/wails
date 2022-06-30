41d1bf3d109a002b7dbb45cb27dd3cdfac2f38cf
stffabi <stffabi@users.noreply.github.com>
2022-06-30 12:13:06 +0200

    [v2] Improvements to the dev command (#1510)
    * [v2, darwin] Fix nullreference exception when dev watcher failed to start and the user stopped wails dev with ctrl-c
    
    * [v2] Improve starting of dev watcher command
    
    Wails dev now also fails if the dev watcher could not be started. Since
    the dev watcher is used to start e.g. vite to serve the frontend, it does
    not make much sense to let Wails dev be running if the dev
    watcher failed to start.
    
    * [v2] Improve killing and cleanup of debug binary
    
    - Make sure to kill the debug process in all cases even if an error occured.
    - Make sure to kill the last started binary process

--- v2/cmd/wails/internal/commands/dev/dev_other.go
+++ v2/cmd/wails/internal/commands/dev/dev_other.go
@@ -6,6 +6,8 @@ package dev
 import (
 	"os/exec"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func setParentGID(cmd *exec.Cmd) {
@@ -15,6 +17,10 @@ func setParentGID(cmd *exec.Cmd) {
 }
 
 func killProc(cmd *exec.Cmd, devCommand string) {
+	if cmd == nil || cmd.Process == nil {
+		return
+	}
+
 	// Experiencing the same issue on macOS BigSur
 	// I'm using Vite, but I would imagine this could be an issue with Node (npm) in general
 	// Also, after several edit/rebuild cycles any abnormal shutdown (crash or CTRL-C) may still leave Node running
@@ -22,7 +28,7 @@ func killProc(cmd *exec.Cmd, devCommand string) {
 	// Not tested on *nix
 	pgid, err := syscall.Getpgid(cmd.Process.Pid)
 	if err == nil {
-		err := syscall.Kill(-pgid, 15) // note the minus sign
+		err := syscall.Kill(-pgid, unix.SIGTERM) // note the minus sign
 		if err != nil {
 			LogRed("Error from '%s' when attempting to kill the process: %s", devCommand, err.Error())
 		}
