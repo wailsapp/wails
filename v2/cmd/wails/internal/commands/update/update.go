07f3f921eefb528354e3c467b9c52d81bd8ceb8b
Lea Anthony <lea.anthony@gmail.com>
2022-07-31 10:05:30 +1000

    Don't show release notes by default, but show how to view them

--- v2/cmd/wails/internal/commands/update/update.go
+++ v2/cmd/wails/internal/commands/update/update.go
@@ -2,12 +2,11 @@ package update
 
 import (
 	"fmt"
+	"github.com/labstack/gommon/color"
+	"github.com/wailsapp/wails/v2/internal/shell"
 	"io"
 	"log"
 	"os"
-	"sync"
-
-	"github.com/wailsapp/wails/v2/internal/shell"
 
 	"github.com/wailsapp/wails/v2/internal/github"
 
@@ -158,32 +157,16 @@ func updateToVersion(logger *clilogger.CLILogger, targetVersion *github.Semantic
 		log.Fatal("Cannot find home directory! Please file a bug report!")
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	releaseNotes := ""
-	go func() {
-		releaseNotes = github.GetReleaseNotes(targetVersionString)
-		wg.Done()
-	}()
-
 	sout, serr, err := shell.RunCommand(homeDir, "go", "install", "github.com/wailsapp/wails/v2/cmd/wails@"+desiredVersion)
 	if err != nil {
 		logger.Println("Failed.")
 		logger.Println(sout + `\n` + serr)
 		return err
 	}
-	logger.Println("\n")
-
-	wg.Wait()
-
-	logger.Println("Wails CLI updated to " + desiredVersion)
-	logger.Println("Make sure you update your project go.mod file to use " + desiredVersion + ":")
-	logger.Println("  require github.com/wailsapp/wails/v2 " + desiredVersion)
-
-	if releaseNotes != "" {
-		logger.Print("\n%s release notes:\n\n", targetVersionString)
-		logger.Println(releaseNotes)
-	}
+	logger.Println("Done.")
+	logger.Println(color.Green("\nMake sure you update your project go.mod file to use " + desiredVersion + ":"))
+	logger.Println(color.Green("  require github.com/wailsapp/wails/v2 " + desiredVersion))
+	logger.Println(color.Red("\nTo view the release notes, please run `wails show releasenotes`"))
 
 	return nil
 }
