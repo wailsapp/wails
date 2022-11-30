100d66fdbd23e15788f440e5c6cddcf523e4ed96
Lea Anthony <lea.anthony@gmail.com>
2022-11-30 18:50:54 +1100

    Fix signal handler

--- v2/internal/frontend/desktop/linux/frontend.go
+++ v2/internal/frontend/desktop/linux/frontend.go
@@ -9,6 +9,69 @@ package linux
 #include "gtk/gtk.h"
 #include "webkit2/webkit2.h"
 
+// CREDIT: https://github.com/rainycape/magick
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+
+static void fix_signal(int signum)
+{
+    struct sigaction st;
+
+    if (sigaction(signum, NULL, &st) < 0) {
+        goto fix_signal_error;
+    }
+    st.sa_flags |= SA_ONSTACK;
+    if (sigaction(signum, &st,  NULL) < 0) {
+        goto fix_signal_error;
+    }
+    return;
+fix_signal_error:
+        fprintf(stderr, "error fixing handler for signal %d, please "
+                "report this issue to "
+                "https://github.com/wailsapp/wails: %s\n",
+                signum, strerror(errno));
+}
+
+static void install_signal_handlers()
+{
+#if defined(SIGCHLD)
+    fix_signal(SIGCHLD);
+#endif
+#if defined(SIGHUP)
+    fix_signal(SIGHUP);
+#endif
+#if defined(SIGINT)
+    fix_signal(SIGINT);
+#endif
+#if defined(SIGQUIT)
+    fix_signal(SIGQUIT);
+#endif
+#if defined(SIGABRT)
+    fix_signal(SIGABRT);
+#endif
+#if defined(SIGFPE)
+    fix_signal(SIGFPE);
+#endif
+#if defined(SIGTERM)
+    fix_signal(SIGTERM);
+#endif
+#if defined(SIGBUS)
+    fix_signal(SIGBUS);
+#endif
+#if defined(SIGSEGV)
+    fix_signal(SIGSEGV);
+#endif
+#if defined(SIGXCPU)
+    fix_signal(SIGXCPU);
+#endif
+#if defined(SIGXFSZ)
+    fix_signal(SIGXFSZ);
+#endif
+}
+
+
 */
 import "C"
 import (
@@ -115,6 +178,8 @@ func NewFrontend(ctx context.Context, appoptions *options.App, myLogger *logger.
 		result.debug = _debug.(bool)
 	}
 	result.mainWindow = NewWindow(appoptions, result.debug)
+
+	C.install_signal_handlers()
 
 	return result
 }
