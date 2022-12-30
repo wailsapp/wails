e18282273e4bca5f13d8f6e421a9cde38987b4ec
Lea Anthony <lea.anthony@gmail.com>
2022-12-31 09:00:13 +1100

    Add missing imports

--- v2/internal/frontend/desktop/linux/frontend.go
+++ v2/internal/frontend/desktop/linux/frontend.go
@@ -91,6 +91,7 @@ import (
 	"github.com/wailsapp/wails/v2/internal/binding"
 	"github.com/wailsapp/wails/v2/internal/frontend"
 	"github.com/wailsapp/wails/v2/internal/frontend/assetserver"
+	"github.com/wailsapp/wails/v2/internal/frontend/runtime"
 	"github.com/wailsapp/wails/v2/internal/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 )
--- v2/internal/frontend/desktop/windows/frontend.go
+++ v2/internal/frontend/desktop/windows/frontend.go
@@ -27,6 +27,7 @@ import (
 	"github.com/wailsapp/wails/v2/internal/frontend/desktop/windows/win32"
 	"github.com/wailsapp/wails/v2/internal/frontend/desktop/windows/winc"
 	"github.com/wailsapp/wails/v2/internal/frontend/desktop/windows/winc/w32"
+	"github.com/wailsapp/wails/v2/internal/frontend/runtime"
 	"github.com/wailsapp/wails/v2/internal/logger"
 	"github.com/wailsapp/wails/v2/internal/system/operatingsystem"
 	"github.com/wailsapp/wails/v2/pkg/options"
