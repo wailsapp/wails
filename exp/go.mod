f4044ebcd9fbf98166d209c3ab45b14fa90ae208
Lea Anthony <lea.anthony@gmail.com>
2023-01-02 08:23:29 +1100

    Add icon tool

--- exp/go.mod
+++ exp/go.mod
@@ -4,7 +4,9 @@ go 1.19
 
 require (
 	github.com/go-task/task/v3 v3.19.0
+	github.com/jackmordaunt/icns/v2 v2.2.1
 	github.com/leaanthony/clir v1.3.0
+	github.com/leaanthony/winicon v1.0.0
 )
 
 require (
@@ -15,9 +17,11 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-zglob v0.0.4 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/radovskyb/watcher v1.0.7 // indirect
 	github.com/sajari/fuzzy v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20220930202632-ec3f01382ef9 // indirect
+	golang.org/x/image v0.0.0-20200430140353-33d19683fad8 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
--- exp/internal/commands/icon.go
+++ exp/internal/commands/icon.go
@@ -1,12 +1,32 @@
 package commands
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/jackmordaunt/icns/v2"
+
+	"github.com/leaanthony/winicon"
+)
 
 type IconOptions struct {
-	Input string
+	Input string `description:"The input image file"`
+	Sizes string `description:"The sizes to generate in .ico file (comma separated)"`
 
-	WindowsFilename string
-	MacFilename     string
+	WindowsFilename string `description:"The output filename for the Windows icon"`
+	MacFilename     string `description:"The output filename for the Mac icon bundle"`
+}
+
+func (i *IconOptions) Default() *IconOptions {
+	return &IconOptions{
+		Sizes:           "256,128,64,48,32,16",
+		MacFilename:     "icons.icns",
+		WindowsFilename: "icons.ico",
+	}
 }
 
 func Icon(options *IconOptions) error {
@@ -14,5 +34,90 @@ func Icon(options *IconOptions) error {
 		return fmt.Errorf("input is required")
 	}
 
+	if options.WindowsFilename == "" && options.MacFilename == "" {
+		return fmt.Errorf("at least one output filename is required")
+	}
+
+	// Parse sizes
+	sizes, err := parseSizes(options.Sizes)
+	if err != nil {
+		return err
+	}
+
+	iconData, err := os.ReadFile(options.Input)
+	if err != nil {
+		return err
+	}
+
+	if options.WindowsFilename != "" {
+		err := generateWindowsIcon(iconData, sizes, options)
+		if err != nil {
+			return err
+		}
+	}
+
+	if options.MacFilename != "" {
+		err := generateMacIcon(iconData, options)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func parseSizes(sizes string) ([]int, error) {
+	// split the input string by comma and confirm that each one is an integer
+	parsedSizes := strings.Split(sizes, ",")
+	result := make([]int, len(parsedSizes))
+	for i, size := range parsedSizes {
+		s, err := strconv.Atoi(size)
+		if err != nil {
+			return nil, err
+		}
+		if s == 0 {
+			continue
+		}
+		result[i] = s
+	}
+
+	// put all integers in a slice and return
+	return result, nil
+}
+
+func generateMacIcon(iconData []byte, options *IconOptions) error {
+
+	srcImg, _, err := image.Decode(bytes.NewBuffer(iconData))
+	if err != nil {
+		return err
+	}
+
+	dest, err := os.Create(options.MacFilename)
+	if err != nil {
+		return err
+
+	}
+	defer func() {
+		err = dest.Close()
+		if err == nil {
+			return
+		}
+	}()
+	return icns.Encode(dest, srcImg)
+}
+
+func generateWindowsIcon(iconData []byte, sizes []int, options *IconOptions) error {
+
+	var output bytes.Buffer
+
+	err := winicon.GenerateIcon(bytes.NewBuffer(iconData), &output, sizes)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(options.WindowsFilename, output.Bytes(), 0644)
+	if err != nil {
+		return err
+	}
 	return nil
 }
