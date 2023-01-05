0a32c1124e1df87e8d1658fcd8641d1f6c312c16
Zámbó, Levente <levente.zambo@gmail.com>
2023-01-05 06:41:07 +0100

    Add common text clipboard support (#2228)

---
+++ v2/internal/frontend/desktop/darwin/clipboard.go
@@ -0,0 +1,36 @@
+//go:build darwin
+
+package darwin
+
+import (
+	"os/exec"
+)
+
+func (f *Frontend) ClipboardGetText() (string, error) {
+	pasteCmd := exec.Command("pbpaste")
+	out, err := pasteCmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func (f *Frontend) ClipboardSetText(text string) error {
+
+	copyCmd := exec.Command("pbcopy")
+	in, err := copyCmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := copyCmd.Start(); err != nil {
+		return err
+	}
+	if _, err := in.Write([]byte(text)); err != nil {
+		return err
+	}
+	if err := in.Close(); err != nil {
+		return err
+	}
+	return copyCmd.Wait()
+}
