bd184cab85bb57f715b8157e3903fa31d48d1aeb
Lea Anthony <lea.anthony@gmail.com>
2023-02-16 20:28:24 +1100

    [v3] Improve plain example

--- v3/examples/plain/main.go
+++ v3/examples/plain/main.go
@@ -19,20 +19,24 @@ func main() {
 	// Create window
 	app.NewWebviewWindowWithOptions(&application.WebviewWindowOptions{
 		Title: "Plain Bundle",
-		CSS:   `body { background-color: rgba(255, 255, 255, 0); } .main { color: white; margin: 20%; }`,
+		CSS:   `body { background-color: rgba(255, 255, 255, 0); font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif; user-select: none; -ms-user-select: none; -webkit-user-select: none; } .main { color: white; margin: 20%; }`,
 		Mac: application.MacWindow{
 			InvisibleTitleBarHeight: 50,
 			Backdrop:                application.MacBackdropTranslucent,
-			TitleBar:                application.MacTitleBarHiddenInset,
+			TitleBar:                application.MacTitleBarHiddenInsetUnified,
 		},
 
 		URL: "/",
 		Assets: application.AssetOptions{
 			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(`<html><head><title>Plain Bundle</title></head><body><div class="main"><h1>Plain Bundle</h1><p>This is a plain bundle. It has no frontend code but this was Served by the AssetServer's Handler</p></div></body></html>`))
+				w.Write([]byte(`<html><head><title>Plain Bundle</title></head><body><div class="main"><h1>Plain Bundle</h1><p>This is a plain bundle. It has no frontend code but this was Served by the AssetServer's Handler.</p><br/><br/><p data-wml-event="clicked">Clicking this paragraph emits an event...<p></div></body></html>`))
 			}),
 		},
+	})
+
+	app.Events.On("clicked", func(_ *application.CustomEvent) {
+		println("clicked")
 	})
 
 	err := app.Run()
