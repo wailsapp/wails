f1a7f1b7817d62934d64948df2466f0ea474ed10
Lea Anthony <lea.anthony@gmail.com>
2023-03-06 20:54:04 +1100

    Improved bindings generation

--- v3/internal/parser/bindings_test.go
+++ v3/internal/parser/bindings_test.go
@@ -11,6 +11,14 @@ func TestGenerateBindings(t *testing.T) {
 
 	tests := []string{
 		"struct_literal_single",
+		"struct_literal_multiple",
+		"struct_literal_multiple_other",
+		"struct_literal_multiple_files",
+		"function_single",
+		"function_from_imported_package",
+		"variable_single",
+		"variable_single_from_function",
+		"variable_single_from_other_function",
 	}
 	for _, projectDir := range tests {
 		t.Run(projectDir, func(t *testing.T) {
@@ -24,21 +32,28 @@ func TestGenerateBindings(t *testing.T) {
 
 			// Generate Bindings
 			got := GenerateBindings(project.BoundMethods)
-			// Write file to project directory
-			err = os.WriteFile(projectDir+"/bindings.got.js", []byte(got), 0644)
-			if err != nil {
-				t.Errorf("os.WriteFile() error = %v", err)
-				return
-			}
+
 			// Load bindings.js from project directory
 			expected, err := os.ReadFile(projectDir + "/bindings.js")
 			if err != nil {
+				// Write file to project directory
+				err = os.WriteFile(projectDir+"/bindings.got.js", []byte(got), 0644)
+				if err != nil {
+					t.Errorf("os.WriteFile() error = %v", err)
+					return
+				}
 				t.Errorf("os.ReadFile() error = %v", err)
 				return
 			}
 
 			// Compare
 			if diff := cmp.Diff(string(expected), got); diff != "" {
+				// Write file to project directory
+				err = os.WriteFile(projectDir+"/bindings.got.js", []byte(got), 0644)
+				if err != nil {
+					t.Errorf("os.WriteFile() error = %v", err)
+					return
+				}
 				t.Fatalf("GenerateService() mismatch (-want +got):\n%s", diff)
 			}
 		})
