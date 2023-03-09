261555c191e72ab6a80893f85595f50ec5e2eb8f
Lea Anthony <lea.anthony@gmail.com>
2023-03-09 21:26:34 +1100

    Init now adds replace lines to generated projects if the CLI was built locally.

--- v3/internal/flags/init.go
+++ v3/internal/flags/init.go
@@ -5,10 +5,7 @@ type Init struct {
 
 	TemplateName string `name:"t" description:"Name of built-in template to use, path to template or template url" default:"vanilla"`
 	ProjectName  string `name:"n" description:"Name of project" default:""`
-	//CIMode       bool   `name:"ci" description:"CI Mode"`
-	ProjectDir string `name:"d" description:"Project directory" default:"."`
-	Quiet      bool   `name:"q" description:"Suppress output to console"`
-	//InitGit    bool   `name:"g" description:"Initialise git repository"`
-	//IDE        string `name:"ide" description:"Generate IDE project files"`
-	List bool `name:"l" description:"List templates"`
+	ProjectDir   string `name:"d" description:"Project directory" default:"."`
+	Quiet        bool   `name:"q" description:"Suppress output to console"`
+	List         bool   `name:"l" description:"List templates"`
 }
