130fab6c01d8ff9ed53f66c1c2a7b18a8d6879b4
Adam Tenderholt <atenderholt@gmail.com>
2023-03-22 13:15:14 -0700

    Feature/v3 parser: expand TS model generation tests & some fixes (#2485)
    * v3 parser: add tests for model generation
    
    * v3 parser: use single quotes for got model.ts
    
    * v3 parser: fixes for some failing tests
    
    * v3 parser: misc simplification and cleanup
    
    * v3 parser: fix model tests when no structs returned
    
    * v3 parser: fix last failing test case
    
    * Update contributors list
    
    * v3 parser: update README
    
    * Revert "Update contributors list"
    
    This reverts commit f429d2ba890020d7ff0772e6207830508261b489.
    
    * Changelog: add line about my contribution

--- v3/internal/parser/models_test.go
+++ v3/internal/parser/models_test.go
@@ -2,132 +2,76 @@ package parser
 
 import (
 	"github.com/google/go-cmp/cmp"
-	"strings"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-const expected = `
-export namespace main {
-  
-  export class Person {
-    name: string;
-    parent: Person;
-    details: anon1;
-    address: package.Address;
-    
-    static createFrom(source: any = {}) {
-      return new Person(source);
-    }
+func TestGenerateModels(t *testing.T) {
 
-    constructor(source: any = {}) {
-      if ('string' === typeof source) {
-        source = JSON.parse(source);
-      }
-
-      this.name = source["name"]
-      this.parent = source["parent"]
-      this.details = source["details"]
-      this.address = source["address"]
-      
-    }
-  }
-  
-  export class anon1 {
-    age: int;
-    address: string;
-    
-    static createFrom(source: any = {}) {
-      return new anon1(source);
-    }
-
-    constructor(source: any = {}) {
-      if ('string' === typeof source) {
-        source = JSON.parse(source);
-      }
-
-      this.age = source["age"]
-      this.address = source["address"]
-      
-    }
-  }
-  
-}
-`
-
-func TestGenerateClass(t *testing.T) {
-	person := StructDef{
-		Name: "Person",
-		Fields: []*Field{
-			{
-				Name: "Name",
-				Type: &ParameterType{
-					Name: "string",
-				},
-			},
-			{
-				Name: "Parent",
-				Type: &ParameterType{
-					Name:      "Person",
-					IsStruct:  true,
-					IsPointer: true,
-					Package:   "main",
-				},
-			},
-			{
-				Name: "Details",
-				Type: &ParameterType{
-					Name:     "anon1",
-					IsStruct: true,
-					Package:  "main",
-				},
-			},
-			{
-				Name: "Address",
-				Type: &ParameterType{
-					Name:      "Address",
-					IsStruct:  true,
-					IsPointer: true,
-					Package:   "github.com/some/other/package",
-				},
-			},
+	tests := []struct {
+		dir  string
+		want string
+	}{
+		{
+			"testdata/function_single",
+			"",
+		},
+		{
+			"testdata/function_from_imported_package",
+			getFile("testdata/function_from_imported_package/models.ts"),
+		},
+		{
+			"testdata/variable_single",
+			"",
+		},
+		{
+			"testdata/variable_single_from_function",
+			"",
+		},
+		{
+			"testdata/variable_single_from_other_function",
+			getFile("testdata/variable_single_from_other_function/models.ts"),
+		},
+		{
+			"testdata/struct_literal_single",
+			getFile("testdata/struct_literal_single/models.ts"),
+		},
+		{
+			"testdata/struct_literal_multiple",
+			"",
+		},
+		{
+			"testdata/struct_literal_multiple_other",
+			getFile("testdata/struct_literal_multiple_other/models.ts"),
+		},
+		{
+			"testdata/struct_literal_multiple_files",
+			"",
 		},
 	}
-	anon1 := StructDef{
-		Name: "anon1",
-		Fields: []*Field{
-			{
-				Name: "Age",
-				Type: &ParameterType{
-					Name: "int",
-				},
-			},
-			{
-				Name: "Address",
-				Type: &ParameterType{
-					Name: "string",
-				},
-			},
-		},
-	}
+	for _, tt := range tests {
+		t.Run(tt.dir, func(t *testing.T) {
+			// Run parser on directory
+			project, err := ParseProject(tt.dir)
+			if err != nil {
+				t.Fatalf("ParseProject() error = %v", err)
+			}
 
-	var builder strings.Builder
-	models := make(map[string]*StructDef)
-	models["Person"] = &person
-	models["anon1"] = &anon1
-	def := ModelDefinitions{
-		Package: "main",
-		Models:  models,
-	}
+			// Generate Models
+			got, err := GenerateModels(project.Models)
+			if err != nil {
+				t.Fatalf("GenerateModels() error = %v", err)
+			}
 
-	err := GenerateModel(&builder, &def)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	text := builder.String()
-	println("Built string")
-	println(text)
-	if diff := cmp.Diff(expected, text); diff != "" {
-		t.Errorf("GenerateClass() failed:\n" + diff)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				err = os.WriteFile(filepath.Join(tt.dir, "models.got.ts"), []byte(got), 0644)
+				if err != nil {
+					t.Errorf("os.WriteFile() error = %v", err)
+					return
+				}
+				t.Fatalf("GenerateModels() mismatch (-want +got):\n%s", diff)
+			}
+		})
 	}
 }
