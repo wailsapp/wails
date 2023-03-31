b3a9332a5b68a1634befd0dc51a6a52810b48611
stffabi <stffabi@users.noreply.github.com>
2023-03-31 10:08:31 +0200

    [assetserver] Add linux platform implementation (#2543)

--- v2/pkg/assetserver/webview/webkit2_legacy.go
+++ v2/pkg/assetserver/webview/webkit2_legacy.go
@@ -1,6 +1,6 @@
 //go:build linux && !webkit2_36
 
-package linux
+package webview
 
 /*
 #cgo linux pkg-config: gtk+-3.0 webkit2gtk-4.0
@@ -14,11 +14,9 @@ import (
 	"fmt"
 	"net/http"
 	"unsafe"
-
-	"github.com/wailsapp/wails/v2/pkg/assetserver"
 )
 
-const webkit2MinMinorVersion = 0
+const Webkit2MinMinorVersion = 0
 
 func webkit_uri_scheme_request_get_http_method(_ *C.WebKitURISchemeRequest) string {
 	return http.MethodGet
@@ -33,7 +31,7 @@ func webkit_uri_scheme_request_finish(req *C.WebKitURISchemeRequest, code int, h
 		return fmt.Errorf("StatusCodes not supported: %d - %s", code, http.StatusText(code))
 	}
 
-	cMimeType := C.CString(header.Get(assetserver.HeaderContentType))
+	cMimeType := C.CString(header.Get(HeaderContentType))
 	C.webkit_uri_scheme_request_finish(req, stream, C.gint64(streamLength), cMimeType)
 	C.free(unsafe.Pointer(cMimeType))
 	return nil
