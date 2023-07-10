ecbb1359497331affdc259fac831469b40f56af6
Lea Anthony <lea.anthony@gmail.com>
2023-07-10 20:56:07 +1000

    [v3] Update plugin.js

--- v3/plugins/oauth/plugin.js
+++ v3/plugins/oauth/plugin.js
@@ -1,14 +1,602 @@
+function Github() {
+    return wails.Plugin("oauth", "Github");
+}
 
-/**
- * Starts the oauth temporary server.
- * @returns {Promise<void>}
- */
-function Start() {
-    return wails.Plugin("oauth", "Start");
+function LogoutGithub() {
+    return wails.Plugin("oauth", "LogoutGithub");
+}
+
+function Amazon() {
+    return wails.Plugin("oauth", "Amazon");
+}
+
+function LogoutAmazon() {
+    return wails.Plugin("oauth", "LogoutAmazon");
+}
+
+function Apple() {
+    return wails.Plugin("oauth", "Apple");
+}
+
+function LogoutApple() {
+    return wails.Plugin("oauth", "LogoutApple");
+}
+
+function Auth0() {
+    return wails.Plugin("oauth", "Auth0");
+}
+
+function LogoutAuth0() {
+    return wails.Plugin("oauth", "LogoutAuth0");
+}
+
+function AzureAD() {
+    return wails.Plugin("oauth", "AzureAD");
+}
+
+function LogoutAzureAD() {
+    return wails.Plugin("oauth", "LogoutAzureAD");
+}
+
+function BattleNet() {
+    return wails.Plugin("oauth", "BattleNet");
+}
+
+function LogoutBattleNet() {
+    return wails.Plugin("oauth", "LogoutBattleNet");
+}
+
+function Bitbucket() {
+    return wails.Plugin("oauth", "Bitbucket");
+}
+
+function LogoutBitbucket() {
+    return wails.Plugin("oauth", "LogoutBitbucket");
+}
+
+function Box() {
+    return wails.Plugin("oauth", "Box");
+}
+
+function LogoutBox() {
+    return wails.Plugin("oauth", "LogoutBox");
+}
+
+function Dailymotion() {
+    return wails.Plugin("oauth", "Dailymotion");
+}
+
+function LogoutDailymotion() {
+    return wails.Plugin("oauth", "LogoutDailymotion");
+}
+
+function Deezer() {
+    return wails.Plugin("oauth", "Deezer");
+}
+
+function LogoutDeezer() {
+    return wails.Plugin("oauth", "LogoutDeezer");
+}
+
+function DigitalOcean() {
+    return wails.Plugin("oauth", "DigitalOcean");
+}
+
+function LogoutDigitalOcean() {
+    return wails.Plugin("oauth", "LogoutDigitalOcean");
+}
+
+function Discord() {
+    return wails.Plugin("oauth", "Discord");
+}
+
+function LogoutDiscord() {
+    return wails.Plugin("oauth", "LogoutDiscord");
+}
+
+function Dropbox() {
+    return wails.Plugin("oauth", "Dropbox");
+}
+
+function LogoutDropbox() {
+    return wails.Plugin("oauth", "LogoutDropbox");
+}
+
+function EveOnline() {
+    return wails.Plugin("oauth", "EveOnline");
+}
+
+function LogoutEveOnline() {
+    return wails.Plugin("oauth", "LogoutEveOnline");
+}
+
+function Facebook() {
+    return wails.Plugin("oauth", "Facebook");
+}
+
+function LogoutFacebook() {
+    return wails.Plugin("oauth", "LogoutFacebook");
+}
+
+function Fitbit() {
+    return wails.Plugin("oauth", "Fitbit");
+}
+
+function LogoutFitbit() {
+    return wails.Plugin("oauth", "LogoutFitbit");
+}
+
+function Gitea() {
+    return wails.Plugin("oauth", "Gitea");
+}
+
+function LogoutGitea() {
+    return wails.Plugin("oauth", "LogoutGitea");
+}
+
+function Gitlab() {
+    return wails.Plugin("oauth", "Gitlab");
+}
+
+function LogoutGitlab() {
+    return wails.Plugin("oauth", "LogoutGitlab");
+}
+
+function Google() {
+    return wails.Plugin("oauth", "Google");
+}
+
+function LogoutGoogle() {
+    return wails.Plugin("oauth", "LogoutGoogle");
+}
+
+function GooglePlus() {
+    return wails.Plugin("oauth", "GooglePlus");
+}
+
+function LogoutGooglePlus() {
+    return wails.Plugin("oauth", "LogoutGooglePlus");
+}
+
+function Heroku() {
+    return wails.Plugin("oauth", "Heroku");
+}
+
+function LogoutHeroku() {
+    return wails.Plugin("oauth", "LogoutHeroku");
+}
+
+function Intercom() {
+    return wails.Plugin("oauth", "Intercom");
+}
+
+function LogoutIntercom() {
+    return wails.Plugin("oauth", "LogoutIntercom");
+}
+
+function Instagram() {
+    return wails.Plugin("oauth", "Instagram");
+}
+
+function LogoutInstagram() {
+    return wails.Plugin("oauth", "LogoutInstagram");
+}
+
+function Kakao() {
+    return wails.Plugin("oauth", "Kakao");
+}
+
+function LogoutKakao() {
+    return wails.Plugin("oauth", "LogoutKakao");
+}
+
+function LastFM() {
+    return wails.Plugin("oauth", "LastFM");
+}
+
+function LogoutLastFM() {
+    return wails.Plugin("oauth", "LogoutLastFM");
+}
+
+function LinkedIn() {
+    return wails.Plugin("oauth", "LinkedIn");
+}
+
+function LogoutLinkedIn() {
+    return wails.Plugin("oauth", "LogoutLinkedIn");
+}
+
+function Line() {
+    return wails.Plugin("oauth", "Line");
+}
+
+function LogoutLine() {
+    return wails.Plugin("oauth", "LogoutLine");
+}
+
+function Mastodon() {
+    return wails.Plugin("oauth", "Mastodon");
+}
+
+function LogoutMastodon() {
+    return wails.Plugin("oauth", "LogoutMastodon");
+}
+
+function Meetup() {
+    return wails.Plugin("oauth", "Meetup");
+}
+
+function LogoutMeetup() {
+    return wails.Plugin("oauth", "LogoutMeetup");
+}
+
+function MicrosoftOnline() {
+    return wails.Plugin("oauth", "MicrosoftOnline");
+}
+
+function LogoutMicrosoftOnline() {
+    return wails.Plugin("oauth", "LogoutMicrosoftOnline");
+}
+
+function Naver() {
+    return wails.Plugin("oauth", "Naver");
+}
+
+function LogoutNaver() {
+    return wails.Plugin("oauth", "LogoutNaver");
+}
+
+function NextCloud() {
+    return wails.Plugin("oauth", "NextCloud");
+}
+
+function LogoutNextCloud() {
+    return wails.Plugin("oauth", "LogoutNextCloud");
+}
+
+function Okta() {
+    return wails.Plugin("oauth", "Okta");
+}
+
+function LogoutOkta() {
+    return wails.Plugin("oauth", "LogoutOkta");
+}
+
+function Onedrive() {
+    return wails.Plugin("oauth", "Onedrive");
+}
+
+function LogoutOnedrive() {
+    return wails.Plugin("oauth", "LogoutOnedrive");
+}
+
+function OpenIDConnect() {
+    return wails.Plugin("oauth", "OpenIDConnect");
+}
+
+function LogoutOpenIDConnect() {
+    return wails.Plugin("oauth", "LogoutOpenIDConnect");
+}
+
+function Patreon() {
+    return wails.Plugin("oauth", "Patreon");
+}
+
+function LogoutPatreon() {
+    return wails.Plugin("oauth", "LogoutPatreon");
+}
+
+function PayPal() {
+    return wails.Plugin("oauth", "PayPal");
+}
+
+function LogoutPayPal() {
+    return wails.Plugin("oauth", "LogoutPayPal");
+}
+
+function SalesForce() {
+    return wails.Plugin("oauth", "SalesForce");
+}
+
+function LogoutSalesForce() {
+    return wails.Plugin("oauth", "LogoutSalesForce");
+}
+
+function SeaTalk() {
+    return wails.Plugin("oauth", "SeaTalk");
+}
+
+function LogoutSeaTalk() {
+    return wails.Plugin("oauth", "LogoutSeaTalk");
+}
+
+function Shopify() {
+    return wails.Plugin("oauth", "Shopify");
+}
+
+function LogoutShopify() {
+    return wails.Plugin("oauth", "LogoutShopify");
+}
+
+function Slack() {
+    return wails.Plugin("oauth", "Slack");
+}
+
+function LogoutSlack() {
+    return wails.Plugin("oauth", "LogoutSlack");
+}
+
+function SoundCloud() {
+    return wails.Plugin("oauth", "SoundCloud");
+}
+
+function LogoutSoundCloud() {
+    return wails.Plugin("oauth", "LogoutSoundCloud");
+}
+
+function Spotify() {
+    return wails.Plugin("oauth", "Spotify");
+}
+
+function LogoutSpotify() {
+    return wails.Plugin("oauth", "LogoutSpotify");
+}
+
+function Steam() {
+    return wails.Plugin("oauth", "Steam");
+}
+
+function LogoutSteam() {
+    return wails.Plugin("oauth", "LogoutSteam");
+}
+
+function Strava() {
+    return wails.Plugin("oauth", "Strava");
+}
+
+function LogoutStrava() {
+    return wails.Plugin("oauth", "LogoutStrava");
+}
+
+function Stripe() {
+    return wails.Plugin("oauth", "Stripe");
+}
+
+function LogoutStripe() {
+    return wails.Plugin("oauth", "LogoutStripe");
+}
+
+function TikTok() {
+    return wails.Plugin("oauth", "TikTok");
+}
+
+function LogoutTikTok() {
+    return wails.Plugin("oauth", "LogoutTikTok");
+}
+
+function Twitter() {
+    return wails.Plugin("oauth", "Twitter");
+}
+
+function LogoutTwitter() {
+    return wails.Plugin("oauth", "LogoutTwitter");
+}
+
+function TwitterV2() {
+    return wails.Plugin("oauth", "TwitterV2");
+}
+
+function LogoutTwitterV2() {
+    return wails.Plugin("oauth", "LogoutTwitterV2");
+}
+
+function Typetalk() {
+    return wails.Plugin("oauth", "Typetalk");
+}
+
+function LogoutTypetalk() {
+    return wails.Plugin("oauth", "LogoutTypetalk");
+}
+
+function Twitch() {
+    return wails.Plugin("oauth", "Twitch");
+}
+
+function LogoutTwitch() {
+    return wails.Plugin("oauth", "LogoutTwitch");
+}
+
+function Uber() {
+    return wails.Plugin("oauth", "Uber");
+}
+
+function LogoutUber() {
+    return wails.Plugin("oauth", "LogoutUber");
+}
+
+function VK() {
+    return wails.Plugin("oauth", "VK");
+}
+
+function LogoutVK() {
+    return wails.Plugin("oauth", "LogoutVK");
+}
+
+function WeCom() {
+    return wails.Plugin("oauth", "WeCom");
+}
+
+function LogoutWeCom() {
+    return wails.Plugin("oauth", "LogoutWeCom");
+}
+
+function Wepay() {
+    return wails.Plugin("oauth", "Wepay");
+}
+
+function LogoutWepay() {
+    return wails.Plugin("oauth", "LogoutWepay");
+}
+
+function Xero() {
+    return wails.Plugin("oauth", "Xero");
+}
+
+function LogoutXero() {
+    return wails.Plugin("oauth", "LogoutXero");
+}
+
+function Yahoo() {
+    return wails.Plugin("oauth", "Yahoo");
+}
+
+function LogoutYahoo() {
+    return wails.Plugin("oauth", "LogoutYahoo");
+}
+
+function Yammer() {
+    return wails.Plugin("oauth", "Yammer");
+}
+
+function LogoutYammer() {
+    return wails.Plugin("oauth", "LogoutYammer");
+}
+
+function Yandex() {
+    return wails.Plugin("oauth", "Yandex");
+}
+
+function LogoutYandex() {
+    return wails.Plugin("oauth", "LogoutYandex");
+}
+
+function Zoom() {
+    return wails.Plugin("oauth", "Zoom");
+}
+
+function LogoutZoom() {
+    return wails.Plugin("oauth", "LogoutZoom");
 }
 
 export default {
-    OAuth: {
-        Start,
-    }
+    Github,
+    LogoutGithub,
+    Amazon,
+    LogoutAmazon,
+    Apple,
+    LogoutApple,
+    Auth0,
+    LogoutAuth0,
+    AzureAD,
+    LogoutAzureAD,
+    BattleNet,
+    LogoutBattleNet,
+    Bitbucket,
+    LogoutBitbucket,
+    Box,
+    LogoutBox,
+    Dailymotion,
+    LogoutDailymotion,
+    Deezer,
+    LogoutDeezer,
+    DigitalOcean,
+    LogoutDigitalOcean,
+    Discord,
+    LogoutDiscord,
+    Dropbox,
+    LogoutDropbox,
+    EveOnline,
+    LogoutEveOnline,
+    Facebook,
+    LogoutFacebook,
+    Fitbit,
+    LogoutFitbit,
+    Gitea,
+    LogoutGitea,
+    Gitlab,
+    LogoutGitlab,
+    Google,
+    LogoutGoogle,
+    GooglePlus,
+    LogoutGooglePlus,
+    Heroku,
+    LogoutHeroku,
+    Intercom,
+    LogoutIntercom,
+    Instagram,
+    LogoutInstagram,
+    Kakao,
+    LogoutKakao,
+    LastFM,
+    LogoutLastFM,
+    LinkedIn,
+    LogoutLinkedIn,
+    Line,
+    LogoutLine,
+    Mastodon,
+    LogoutMastodon,
+    Meetup,
+    LogoutMeetup,
+    MicrosoftOnline,
+    LogoutMicrosoftOnline,
+    Naver,
+    LogoutNaver,
+    NextCloud,
+    LogoutNextCloud,
+    Okta,
+    LogoutOkta,
+    Onedrive,
+    LogoutOnedrive,
+    OpenIDConnect,
+    LogoutOpenIDConnect,
+    Patreon,
+    LogoutPatreon,
+    PayPal,
+    LogoutPayPal,
+    SalesForce,
+    LogoutSalesForce,
+    SeaTalk,
+    LogoutSeaTalk,
+    Shopify,
+    LogoutShopify,
+    Slack,
+    LogoutSlack,
+    SoundCloud,
+    LogoutSoundCloud,
+    Spotify,
+    LogoutSpotify,
+    Steam,
+    LogoutSteam,
+    Strava,
+    LogoutStrava,
+    Stripe,
+    LogoutStripe,
+    TikTok,
+    LogoutTikTok,
+    Twitter,
+    LogoutTwitter,
+    TwitterV2,
+    LogoutTwitterV2,
+    Typetalk,
+    LogoutTypetalk,
+    Twitch,
+    LogoutTwitch,
+    Uber,
+    LogoutUber,
+    VK,
+    LogoutVK,
+    WeCom,
+    LogoutWeCom,
+    Wepay,
+    LogoutWepay,
+    Xero,
+    LogoutXero,
+    Yahoo,
+    LogoutYahoo,
+    Yammer,
+    LogoutYammer,
+    Yandex,
+    LogoutYandex,
+    Zoom,
+    LogoutZoom
 };
