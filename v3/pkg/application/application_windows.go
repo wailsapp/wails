841289f1d1a3d2ce3e0c0404b7d05da3812121ac
Lea Anthony <lea.anthony@gmail.com>
2023-08-18 19:50:57 +1000

    [v3 windows] Support `isDarkMode`. Add common application event mapping

--- v3/pkg/application/application_windows.go
+++ v3/pkg/application/application_windows.go
@@ -178,6 +178,7 @@ func (m *windowsApp) setApplicationMenu(menu *Menu) {
 }
 
 func (m *windowsApp) run() error {
+	m.setupCommonEvents()
 	for eventID := range m.parent.applicationEventListeners {
 		m.on(eventID)
 	}
---
+++ v3/pkg/application/events_common_windows.go
@@ -0,0 +1,17 @@
+//go:build windows
+
+package application
+
+import "github.com/wailsapp/wails/v3/pkg/events"
+
+var commonApplicationEventMap = map[events.ApplicationEventType]events.ApplicationEventType{
+	events.Windows.SystemThemeChanged: events.Common.ThemeChanged,
+}
+
+func (m *windowsApp) setupCommonEvents() {
+	for sourceEvent, targetEvent := range commonApplicationEventMap {
+		m.parent.On(sourceEvent, func(event *Event) {
+			applicationEvents <- uint(targetEvent)
+		})
+	}
+}
