bc4dcff124ab8947ffc5d6ef4cad601e7a4e9123
Fadi Khadra <fdkhadra@gmail.com>
2023-09-24 11:32:29 +0200

    bump u and update preferences

--- v2/pkg/options/mac/preferences.go
+++ v2/pkg/options/mac/preferences.go
@@ -2,6 +2,9 @@ package mac
 
 import "github.com/leaanthony/u"
 
+var Enabled = u.True
+var Disabled = u.False
+
 // Preferences allows to set webkit preferences
 type Preferences struct {
 	// A Boolean value that indicates whether pressing the tab key changes the focus to links and form controls.
@@ -10,12 +13,4 @@ type Preferences struct {
 	// A Boolean value that indicates whether to allow people to select or otherwise interact with text.
 	// Set to true by default.
 	TextInteractionEnabled u.Bool
-}
-
-func EnablePreference() u.Bool {
-	return u.NewBool(true)
-}
-
-func DisablePreference() u.Bool {
-	return u.NewBool(true)
 }
