130aab359812b9b3124b6fd2def8a5b7594bf707
Travis McLane <tmclane@gmail.com>
2023-10-05 17:25:45 -0500

    [v3 linux] systray stubout
    slim down the printouts and fix compilation

--- v3/pkg/application/systemtray_linux.go
+++ v3/pkg/application/systemtray_linux.go
@@ -2,8 +2,6 @@
 
 package application
 
-import "log"
-
 type linuxSystemTray struct {
 	id    uint
 	label string
@@ -16,44 +14,39 @@ type linuxSystemTray struct {
 
 func (s *linuxSystemTray) setIconPosition(position int) {
 	s.iconPosition = position
-	log.Println("linuxSystemTray.setIconPosition() not implemented")
 }
 
 func (s *linuxSystemTray) setMenu(menu *Menu) {
 	s.menu = menu
-	log.Println("linuxSystemTray.setMenu() not implemented")
 }
 
 func (s *linuxSystemTray) positionWindow(window *WebviewWindow, offset int) error {
-	log.Println("linuxSystemTray.positionWindow() not implemented")
+	return nil
 }
 
 func (s *linuxSystemTray) getScreen() (*Screen, error) {
-	log.Println("linuxSystemTray.getScreen() not implemented")
+	return &Screen{}, nil
 }
 
 func (s *linuxSystemTray) bounds() (*Rect, error) {
-	log.Println("linuxSystemTray.bounds() not implemented")
+	return &Rect{}, nil
 }
 
 func (s *linuxSystemTray) run() {
-	log.Println("linuxSystemTray.run() - not implemented")
+	globalApplication.error("linuxSystemTray.run() - not implemented")
 }
 
 func (s *linuxSystemTray) setIcon(icon []byte) {
 	s.icon = icon
-	log.Println("linuxSystemTray.setIcon() not implemented")
 }
 
 func (s *linuxSystemTray) setDarkModeIcon(icon []byte) {
 	s.icon = icon
-	log.Println("linuxSystemTray.setDarkModeIcon() not implemented")
 }
 
 func (s *linuxSystemTray) setTemplateIcon(icon []byte) {
 	s.icon = icon
 	s.isTemplateIcon = true
-	log.Println("linuxSystemTray.setTemplateIcon() not implemented")
 }
 
 func newSystemTrayImpl(s *SystemTray) systemTrayImpl {
@@ -68,15 +61,12 @@ func newSystemTrayImpl(s *SystemTray) systemTrayImpl {
 }
 
 func (s *linuxSystemTray) openMenu() {
-	log.Println("linuxSystemTray.openMenu() not implemented")
 }
 
 func (s *linuxSystemTray) setLabel(label string) {
 	s.label = label
-	log.Println("linuxSystemTray.setLabel() not implemented")
 }
 
 func (s *linuxSystemTray) destroy() {
 	// Remove the status item from the status bar and its associated menu
-	log.Println("linuxSystemTray.destroy() not implemented")
 }
