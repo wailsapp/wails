ae38d1e0208e37b75edfa4fb085d8ea36df2693f
Lea Anthony <lea.anthony@gmail.com>
2023-11-12 16:35:44 +1100

    Fix taskfile in templates. Sanitize the project name

--- v3/internal/commands/init.go
+++ v3/internal/commands/init.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"path/filepath"
+	"regexp"
 
 	"github.com/wailsapp/wails/v3/internal/flags"
 	"github.com/wailsapp/wails/v3/internal/templates"
@@ -22,6 +23,8 @@ func Init(options *flags.Init) error {
 	if options.ProjectName == "" {
 		return fmt.Errorf("please use the -n flag to specify a project name")
 	}
+
+	options.ProjectName = sanitizeFileName(options.ProjectName)
 
 	if !templates.ValidTemplateName(options.TemplateName) {
 		return fmt.Errorf("invalid template name: %s. Use -l flag to list valid templates", options.TemplateName)
@@ -53,4 +56,13 @@ func printTemplates() error {
 	err := pterm.DefaultTable.WithHasHeader(true).WithBoxed(true).WithData(table).Render()
 	pterm.Println()
 	return err
+}
+
+func sanitizeFileName(fileName string) string {
+	// Regular expression to match non-allowed characters in file names
+	// You can adjust this based on the specific requirements of your file system
+	reg := regexp.MustCompile(`[^a-zA-Z0-9_.-]`)
+
+	// Replace matched characters with an underscore or any other safe character
+	return reg.ReplaceAllString(fileName, "_")
 }
