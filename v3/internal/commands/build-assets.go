022baad9e541344091bf6f33ddca74c21cae3256
Lea Anthony <lea.anthony@gmail.com>
2023-11-17 21:53:45 +1100

    Update default taskfile. Update copyright symbol

--- v3/internal/commands/build-assets.go
+++ v3/internal/commands/build-assets.go
@@ -22,7 +22,7 @@ type BuildAssetsOptions struct {
 	ProductDescription string `description:"The description of the product" default:"My Product Description"`
 	ProductVersion     string `description:"The version of the product" default:"0.1.0"`
 	ProductCompany     string `description:"The company of the product" default:"My Company"`
-	ProductCopyright   string `description:"The copyright notice" default:"(c) now, My Company"`
+	ProductCopyright   string `description:"The copyright notice" default:"\u00a9 now, My Company"`
 	ProductComments    string `description:"Comments to add to the generated files" default:"This is a comment"`
 	ProductIdentifier  string `description:"The product identifier, e.g com.mycompany.myproduct"`
 	Silent             bool   `description:"Suppress output to console"`
