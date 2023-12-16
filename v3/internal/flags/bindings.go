182f43004a9173613cd12bf595681ab763468178
Lea Anthony <lea.anthony@gmail.com>
2023-12-16 14:02:19 +1100

    Improved bindings generation: support JS Models.

--- v3/internal/flags/bindings.go
+++ v3/internal/flags/bindings.go
@@ -3,6 +3,7 @@ package flags
 type GenerateBindingsOptions struct {
 	Silent           bool   `name:"silent" description:"Silent mode"`
 	ModelsFilename   string `name:"m" description:"The filename for the models file" default:"models.ts"`
+	TS               bool   `name:"ts" description:"Generate Typescript bindings"`
 	TSPrefix         string `description:"The prefix for the typescript names" default:""`
 	TSSuffix         string `description:"The postfix for the typescript names" default:""`
 	UseInterfaces    bool   `name:"i" description:"Use interfaces instead of classes"`
