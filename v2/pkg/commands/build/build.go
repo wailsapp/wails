4b4fcdd47cb193ff20ea55a689a30e18809d8d9e
Light <lochois@gmail.com>
2023-12-18 07:30:05 +1100

    Added possibility to specify platform for dev command (#3117)
    * Added possibility to specify platform for dev command
    
    ---------
    
    Co-authored-by: Lea Anthony <lea.anthony@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- v2/pkg/commands/build/build.go
+++ v2/pkg/commands/build/build.go
@@ -71,6 +71,10 @@ type Options struct {
 	SkipBindings      bool                 // Skip binding generation
 }
 
+func (o *Options) IsWindowsTargetPlatform() bool {
+	return strings.Contains(strings.ToLower(o.Platform), "windows")
+}
+
 // Build the project!
 func Build(options *Options) (string, error) {
 	// Extract logger
