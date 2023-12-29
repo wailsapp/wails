8a12a32d314e0c84d094b2d58ab6a8b2ff2f2652
Lea Anthony <lea.anthony@gmail.com>
2023-12-29 12:50:25 +1100

    Vanilla template: Update to use runtime module

--- v3/internal/templates/vanilla/frontend/main.js
+++ v3/internal/templates/vanilla/frontend/main.js
@@ -1,23 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import {Greet} from "./bindings/main/GreetService.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://wails.io" target="_blank">
-      <img src="/wails.png" class="logo" alt="Wails logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Wails!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Wails logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+window.doGreet = () => {
+    Greet('test').then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
