c531c714d4fd4c19a28aa386dceb7a09fde87aed
Lea Anthony <lea.anthony@gmail.com>
2023-12-30 21:11:38 +1100

    Update vanilla+js template and dependencies
    The frontend interaction has been significantly updated, including improvements to the Greet function and the addition of an ongoing time display. Additionally, dependencies in package.json have been updated to their latest versions. Also, redundant print statement was removed from messageprocessor_call.go and changes were made to layout structure and styles in the main.go.tmpl and index.html files.

--- v3/internal/templates/vanilla/frontend/main.js
+++ v3/internal/templates/vanilla/frontend/main.js
@@ -1,9 +1,20 @@
 import {Greet} from "./bindings/main/GreetService.js";
+import {Events} from "@wailsio/runtime";
 
 window.doGreet = () => {
-    Greet('test').then((result) => {
-        console.log(result);
+    let name = document.getElementById('name').value;
+    if (!name) {
+        name = 'from Go';
+    }
+    Greet(name).then((result) => {
+        let element = document.getElementById('greeting');
+        element.innerText = result;
     }).catch((err) => {
         console.log(err);
     });
 }
+
+Events.On('time', (time) => {
+    let element = document.getElementById('time');
+    element.innerText = time.data;
+});
