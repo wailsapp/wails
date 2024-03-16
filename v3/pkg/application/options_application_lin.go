e9a9420e5c5d71faf1d0e7c09feba3c38ec640f9
Lea Anthony <lea.anthony@gmail.com>
2024-03-17 09:42:27 +1100

    Refactor options. Update deps

--- v3/pkg/application/options_application_lin.go
+++
@@ -1,7 +0,0 @@
-package application
-
-// LinuxOptions contains options for Linux applications.
-type LinuxOptions struct {
-	// DisableQuitOnLastWindowClosed disables the auto quit of the application if the last window has been closed.
-	DisableQuitOnLastWindowClosed bool
-}
