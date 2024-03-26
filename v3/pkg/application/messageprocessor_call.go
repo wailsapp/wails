a3a3b5fb76cc254a6c36673e6db8541760503267
Fabio Massaioli <fabio.massaioli@gmail.com>
2024-03-26 16:33:11 +0100

    Improve logging of remote method calls

--- v3/pkg/application/messageprocessor_call.go
+++ v3/pkg/application/messageprocessor_call.go
@@ -80,16 +80,16 @@ func (m *MessageProcessor) processCallMethod(method int, rw http.ResponseWriter,
 
 		ctx, cancel := context.WithCancel(context.WithoutCancel(r.Context()))
 
-		ambigiousID := false
+		ambiguousID := false
 		m.l.Lock()
 		if m.runningCalls[*callID] != nil {
-			ambigiousID = true
+			ambiguousID = true
 		} else {
 			m.runningCalls[*callID] = cancel
 		}
 		m.l.Unlock()
 
-		if ambigiousID {
+		if ambiguousID {
 			cancel()
 			m.callErrorCallback(window, "Error calling method: %s, a method call with the same id is already running", callID, err)
 			return
@@ -119,7 +119,12 @@ func (m *MessageProcessor) processCallMethod(method int, rw http.ResponseWriter,
 				}
 			}
 			m.callCallback(window, callID, string(jsonResult), true)
-			m.Info("Call Binding:", "method", boundMethod, "args", options.Args, "result", result)
+
+			var jsonArgs struct {
+				Args json.RawMessage `json:"args"`
+			}
+			params.ToStruct(&jsonArgs)
+			m.Info("Call Binding:", "method", boundMethod, "args", string(jsonArgs.Args), "result", result)
 		}()
 		m.ok(rw)
 	default:
