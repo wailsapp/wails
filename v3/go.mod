d6a83b3684372a5bb6e7669aa10cdb025233212f
Fabio Massaioli <fabio.massaioli@gmail.com>
2024-05-08 16:54:06 +0200

    Update typeutil package and remove workaround

--- v3/go.mod
+++ v3/go.mod
@@ -31,7 +31,8 @@ require (
 	github.com/tc-hib/winres v0.1.6
 	github.com/wailsapp/go-webview2 v1.0.9
 	github.com/wailsapp/mimetype v1.4.1
-	golang.org/x/sys v0.19.0
+	golang.org/x/sys v0.20.0
+	golang.org/x/tools v0.21.0
 	modernc.org/sqlite v1.21.0
 )
 
@@ -80,15 +81,14 @@ require (
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
 	golang.org/x/image v0.15.0 // indirect
-	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/term v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- v3/internal/parser/render/module.go
+++ v3/internal/parser/render/module.go
@@ -1,8 +1,6 @@
 package render
 
 import (
-	"go/types"
-
 	"github.com/wailsapp/wails/v3/internal/flags"
 	"github.com/wailsapp/wails/v3/internal/parser/collect"
 	"golang.org/x/tools/go/types/typeutil"
@@ -15,7 +13,7 @@ type module struct {
 
 	Imports *collect.ImportMap
 
-	postponedCreates typeMapWorkaround
+	postponedCreates typeutil.Map
 }
 
 // Runtime returns the import path for the Wails JS runtime module.
@@ -24,47 +22,5 @@ func (m *module) Runtime() string {
 		return "/wails/runtime.js"
 	} else {
 		return "@wailsio/runtime"
-	}
-}
-
-// typeutil.Map has a bug where struct fields with alias types result in panics.
-// typeMapWorkaround is a drop-in workaround that offers the interface we need.
-// It should be replaced by typeutil.Map as soon as the bug is fixed.
-type typeMapWorkaround struct {
-	main     typeutil.Map
-	fallback map[types.Type]any
-}
-
-func (cm *typeMapWorkaround) Len() int {
-	return cm.main.Len() + len(cm.fallback)
-}
-
-func (cm *typeMapWorkaround) At(typ types.Type) (result any) {
-	defer func() {
-		if recover() != nil {
-			result = cm.fallback[typ]
-		}
-	}()
-
-	return cm.main.At(typ)
-}
-
-func (cm *typeMapWorkaround) Set(typ types.Type, value any) {
-	defer func() {
-		if recover() != nil {
-			if cm.fallback == nil {
-				cm.fallback = make(map[types.Type]any)
-			}
-			cm.fallback[typ] = value
-		}
-	}()
-
-	cm.main.Set(typ, value)
-}
-
-func (cm *typeMapWorkaround) Iterate(yield func(key types.Type, value any)) {
-	cm.main.Iterate(yield)
-	for key, value := range cm.fallback {
-		yield(key, value)
 	}
 }
