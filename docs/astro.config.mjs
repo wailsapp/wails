5cb44e923e7ba8377eb7fe878bf6219384ebab4e
Stavros Kois <s.kois@outlook.com>
2024-11-27 00:25:21 +0200

    add few categories

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -36,17 +36,18 @@ export default defineConfig({
         starlightImageZoom(),
       ],
       sidebar: [
-        // {
-        //   label: "Guides",
-        //   items: [
-        //     // Each item here is one entry in the navigation menu.
-        //     { label: "Example Guide", slug: "guides/example" },
-        //   ],
-        // },
-        // {
-        //   label: "Reference",
-        //   autogenerate: { directory: "reference" },
-        // },
+        {
+          label: "API",
+          autogenerate: { directory: "api", collapsed: false },
+        },
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started", collapsed: false },
+        },
+        {
+          label: "Development",
+          autogenerate: { directory: "development", collapsed: false },
+        },
       ],
     }),
   ],
