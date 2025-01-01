68b315533a4d51152ad0cc8846cb1fec2f2d28bc
Sebastian Rössel <sebastian-roessel@arcor.de>
2025-01-01 21:43:37 +0100

    added v3 alpha8.3 example of minsize bug under linux (lmde)

---
+++ v3/examples/linux-minsize-bug/main.go
@@ -0,0 +1,28 @@
+ypackage main
+
+import (
+    "log"
+
+    "github.com/wailsapp/wails/v3/pkg/application"
+)
+
+func main() {
+
+    app := application.New(application.Options{
+	Name: "v3-alpha8.3-minsize-linux-bug",
+    })
+
+    app.NewWebviewWindowWithOptions(application.WebviewWindowOptions{
+	Title:     "v3-alpha8.3-minsize-linux-bug",
+	URL:       "/",
+	Width:     1024,
+	Height:    768,
+	MinWidth:  1024,
+	MinHeight: 768,
+    })
+
+    err := app.Run()
+    if err != nil {
+	log.Fatal(err)
+    }
+}
