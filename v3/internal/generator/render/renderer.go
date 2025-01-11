5fcce727387eb96354e0453e83c53af9595416b2
Fabio Massaioli <fabio.massaioli@gmail.com>
2025-01-11 09:10:27 +0100

    Sort class aliases after their aliased class

--- v3/internal/generator/render/renderer.go
+++ v3/internal/generator/render/renderer.go
@@ -1,7 +1,9 @@
 package render
 
 import (
+	"go/types"
 	"io"
+	"slices"
 	"strings"
 	"text/template"
 
@@ -81,6 +83,45 @@ func (renderer *Renderer) Service(w io.Writer, info *collect.ServiceInfo) error 
 
 // Typedefs renders type definitions for the given list of models.
 func (renderer *Renderer) Typedefs(w io.Writer, imports *collect.ImportMap, models []*collect.ModelInfo) error {
+	if !renderer.options.UseInterfaces {
+		// Sort class aliases after the class they alias.
+		// Works in amortized linear time thanks to an auxiliary map.
+
+		// Track postponed class aliases and their dependencies.
+		aliases := make(map[types.Object][]*collect.ModelInfo, len(models))
+		// Placeholder for marking visited objects.
+		visited := make([]*collect.ModelInfo, 0)
+
+		models = slices.Clone(models)
+		for i, j := 0, 0; i < len(models); i++ {
+			if models[i].Type != nil && collect.IsClass(models[i].Type) {
+				// models[i] is a class alias:
+				// models[i].Type is guaranteed to be
+				// either an alias or a named type
+				obj := models[i].Type.(interface{ Obj() *types.TypeName }).Obj()
+				if obj.Pkg().Path() == imports.Self {
+					// models[i] aliases a type from the current module.
+					if a := aliases[obj]; a == nil || len(a) > 0 {
+						// The aliased type has not been visited already, postpone.
+						aliases[obj] = append(a, models[i])
+						continue
+					}
+				}
+			}
+
+			// Append models[i].
+			models[j] = models[i]
+			j++
+
+			// Keep appending aliases whose aliased type has been just appended.
+			for k := j - 1; k < j; k++ {
+				a := aliases[models[k].Object()]
+				aliases[models[k].Object()] = visited
+				j += copy(models[j:], a)
+			}
+		}
+	}
+
 	return renderer.typedefs.Execute(w, &struct {
 		module
 		Models []*collect.ModelInfo
