cb51682259466b4796219b3fd69e345296f7f22a
Fabio Massaioli <fabio.massaioli@gmail.com>
2025-01-11 09:35:22 +0100

    Test class aliases

--- v3/internal/generator/testcases/aliases/main.go
+++ v3/internal/generator/testcases/aliases/main.go
@@ -10,8 +10,11 @@ import (
 // GreetService is great
 type GreetService int
 
-// A nice type Alias
+// A nice type Alias.
 type Alias = int
+
+// A class alias.
+type AliasedPerson = Person
 
 // An empty struct alias.
 type EmptyAliasStruct = struct{}
@@ -40,20 +43,28 @@ type Person struct {
 	AliasedField Alias  // A random alias field.
 }
 
-// A generic struct containing an alias
+// A generic struct containing an alias.
 type GenericPerson[T any] struct {
 	Name         T
 	AliasedField Alias
 }
 
-// Get someone
+// Another class alias, but ordered after its aliased class.
+type StrangelyAliasedPerson = Person
+
+// Get someone.
 func (GreetService) Get(aliasValue Alias) Person {
 	return Person{"hello", aliasValue}
 }
 
-// Get someone quite different
+// Get someone quite different.
 func (GreetService) GetButDifferent() GenericPerson[bool] {
 	return GenericPerson[bool]{true, 13}
+}
+
+// Apparently, aliases are all the rage right now.
+func (GreetService) GetButAliased(p AliasedPerson) StrangelyAliasedPerson {
+	return p
 }
 
 // Greet a lot of unusual things.
