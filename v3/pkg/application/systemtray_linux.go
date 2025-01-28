d68e21b6b66d52a47a9a7ef5a8b286d0cee6a7eb
Lea Anthony <lea.anthony@gmail.com>
2025-01-28 21:12:36 +1100

    Fix build error

--- v3/pkg/application/systemtray_linux.go
+++ v3/pkg/application/systemtray_linux.go
@@ -86,6 +86,8 @@ func (s *systrayMenuItem) setDisabled(disabled bool) {
 	}
 }
 
+func (s *systrayMenuItem) destroy() {}
+
 func (s *systrayMenuItem) setChecked(checked bool) {
 	v := dbus.MakeVariant(0)
 	if checked {
