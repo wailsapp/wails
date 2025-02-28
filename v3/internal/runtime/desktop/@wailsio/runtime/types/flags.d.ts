7c56c4ce007510b46350f9ec20cca2cefa61b085
Fabio Massaioli <fabio.massaioli@gmail.com>
2025-02-28 22:22:41 +0100

    Remove build artifacts

--- v3/internal/runtime/desktop/@wailsio/runtime/types/flags.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Retrieves the value associated with the specified key from the flag map.
- *
- * @param key - The key to retrieve the value for.
- * @return The value associated with the specified key.
- */
-export declare function GetFlag(key: string): any;
--- v3/internal/runtime/desktop/@wailsio/runtime/types/wml.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Schedules an automatic reload of WML to be performed as soon as the document is fully loaded.
- */
-export declare function Enable(): void;
-/**
- * Reloads the WML page by adding necessary event listeners and browser listeners.
- */
-export declare function Reload(): void;
