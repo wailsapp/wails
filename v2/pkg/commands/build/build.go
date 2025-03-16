ecf1c41db49da05e0db3cbac8341710408618bee
josc146 <josStorer@outlook.com>
2025-03-16 19:32:31 +0800

    improve comment

--- v2/pkg/commands/build/build.go
+++ v2/pkg/commands/build/build.go
@@ -69,7 +69,7 @@ type Options struct {
 	Obfuscated        bool                 // Indicates that bound methods should be obfuscated
 	GarbleArgs        string               // The arguments for Garble
 	SkipBindings      bool                 // Skip binding generation
-	SkipEmbedCreate   bool                 // Skip embed creation when in development mode
+	SkipEmbedCreate   bool                 // Skip creation of embed files
 }
 
 // Build the project!
