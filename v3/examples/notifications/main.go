b102279630d048b0a26aca0fd9282f2992dd282b
popaprozac <zbotterman@gmail.com>
2025-03-24 16:05:57 -0700

    update example

--- v3/examples/notifications/main.go
+++ v3/examples/notifications/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"embed"
 	_ "embed"
-	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -12,17 +11,31 @@ import (
 	"github.com/wailsapp/wails/v3/pkg/services/notifications"
 )
 
+// Wails uses Go's `embed` package to embed the frontend files into the binary.
+// Any files in the frontend/dist folder will be embedded into the binary and
+// made available to the frontend.
+// See https://pkg.go.dev/embed for more information.
+
 //go:embed all:frontend/dist
 var assets embed.FS
 
+// main function serves as the application's entry point. It initializes the application, creates a window,
+// and starts a goroutine that emits a time-based event every second. It subsequently runs the application and
+// logs any error that might occur.
 func main() {
-	notificationService := notifications.New()
+	// Create a new Notification Service
+	ns := notifications.New()
 
+	// Create a new Wails application by providing the necessary options.
+	// Variables 'Name' and 'Description' are for application metadata.
+	// 'Assets' configures the asset server with the 'FS' variable pointing to the frontend files.
+	// 'Bind' is a list of Go struct instances. The frontend has access to the methods of these instances.
+	// 'Mac' options tailor the application when running an macOS.
 	app := application.New(application.Options{
-		Name:        "Notifications",
-		Description: "A demo of using raw HTML & CSS",
+		Name:        "Notifications Demo",
+		Description: "A demo of using desktop notifications with Wails",
 		Services: []application.Service{
-			application.NewService(notificationService),
+			application.NewService(ns),
 		},
 		Assets: application.AssetOptions{
 			Handler: application.AssetFileServerFS(assets),
@@ -30,13 +43,15 @@ func main() {
 		Mac: application.MacOptions{
 			ApplicationShouldTerminateAfterLastWindowClosed: true,
 		},
-		Windows: application.WindowsOptions{
-			WndClass: "Notifications",
-		},
 	})
 
+	// Create a new window with the necessary options.
+	// 'Title' is the title of the window.
+	// 'Mac' options tailor the window when running on macOS.
+	// 'BackgroundColour' is the background colour of the window.
+	// 'URL' is the URL that will be loaded into the webview.
 	app.NewWebviewWindowWithOptions(application.WebviewWindowOptions{
-		Title: "Notifications",
+		Title: "Window 1",
 		Mac: application.MacWindow{
 			InvisibleTitleBarHeight: 50,
 			Backdrop:                application.MacBackdropTranslucent,
@@ -46,67 +61,90 @@ func main() {
 		URL:              "/",
 	})
 
+	// Create a goroutine that spawns desktop notifications from Go
 	go func() {
-		granted, err := notificationService.RequestNotificationAuthorization()
+		var authorized bool
+		var err error
+		authorized, err = ns.CheckNotificationAuthorization()
 		if err != nil {
-			log.Default().Printf("WARNING: %s\n", err)
-			return
+			println(fmt.Errorf("checking app notification authorization failed: %s", err))
 		}
 
-		if granted {
-			notificationService.OnNotificationResponse(func(result notifications.NotificationResult) {
-				if result.Error != nil {
-					fmt.Printf("notification response error: %s", result.Error)
-					return
-				}
-				data, _ := json.Marshal(result.Response)
-				fmt.Printf("%s\n", string(data))
-				app.EmitEvent("notification:response", result.Response)
-			})
-			time.Sleep(time.Second * 2)
+		if !authorized {
+			authorized, err = ns.RequestNotificationAuthorization()
+			if err != nil {
+				println(fmt.Errorf("requesting app notification authorization failed: %s", err))
+			}
+		}
 
-			notificationService.SendNotification(notifications.NotificationOptions{
-				ID:    "uuid1",
-				Title: "Title!",
-				Body:  "Body!",
+		if authorized {
+			ns.OnNotificationResponse(func(result notifications.NotificationResult) {
+				if result.Error != nil {
+					println(fmt.Errorf("parsing notification result failed: %s", result.Error))
+				} else {
+					fmt.Printf("Response: %+v\n", result.Response)
+					println("Sending response to frontend...")
+					app.EmitEvent("notification:action", result.Response)
+				}
+			})
+
+			err = ns.SendNotification(notifications.NotificationOptions{
+				ID:       "uuid-basic-1",
+				Title:    "Notification Title",
+				Subtitle: "Subtitle on macOS and Linux",
+				Body:     "Body text of notification.",
 				Data: map[string]interface{}{
-					"messageId": "msg-123",
-					"senderId":  "user-123",
-					"timestamp": time.Now().Unix(),
+					"user-id":    "user-123",
+					"message-id": "msg-123",
+					"timestamp":  time.Now().Unix(),
 				},
 			})
+			if err != nil {
+				println(fmt.Errorf("sending basic notification failed: %s", err))
+			}
 
+			// Delay before sending next notification
 			time.Sleep(time.Second * 2)
 
-			notificationService.RegisterNotificationCategory(notifications.NotificationCategory{
-				ID: "BACKEND_NOTIF",
+			const CategoryID = "backend-notification-id"
+
+			err = ns.RegisterNotificationCategory(notifications.NotificationCategory{
+				ID: CategoryID,
 				Actions: []notifications.NotificationAction{
-					{ID: "VIEW_ACTION", Title: "View"},
-					{ID: "MARK_READ_ACTION", Title: "Mark as Read"},
-					{ID: "DELETE_ACTION", Title: "Delete", Destructive: true},
+					{ID: "VIEW", Title: "View"},
+					{ID: "MARK_READ", Title: "Mark as read"},
+					{ID: "DELETE", Title: "Delete", Destructive: true},
 				},
 				HasReplyField:    true,
+				ReplyPlaceholder: "Message...",
 				ReplyButtonTitle: "Reply",
-				ReplyPlaceholder: "Reply to backend...",
 			})
+			if err != nil {
+				println(fmt.Errorf("creating notification category failed: %s", err))
+			}
 
-			notificationService.SendNotificationWithActions(notifications.NotificationOptions{
-				ID:         "uuid2",
-				Title:      "Complex Backend Notification",
-				Subtitle:   "From: Jane Doe",
-				Body:       "Is it raining today where you are?",
-				CategoryID: "BACKEND_NOTIF",
+			err = ns.SendNotificationWithActions(notifications.NotificationOptions{
+				ID:         "uuid-with-actions-1",
+				Title:      "Actions Notification Title",
+				Subtitle:   "Subtitle on macOS and Linux",
+				Body:       "Body text of notification with actions.",
+				CategoryID: CategoryID,
 				Data: map[string]interface{}{
-					"messageId": "msg-456",
-					"senderId":  "user-456",
-					"timestamp": time.Now().Unix(),
+					"user-id":    "user-123",
+					"message-id": "msg-123",
+					"timestamp":  time.Now().Unix(),
 				},
 			})
+			if err != nil {
+				println(fmt.Errorf("sending notification with actions failed: %s", err))
+			}
 		}
 	}()
 
+	// Run the application. This blocks until the application has been exited.
 	err := app.Run()
 
+	// If an error occurred while running the application, log it and exit.
 	if err != nil {
 		log.Fatal(err)
 	}
