2a8d4fbeef30ca02eb3e3e09a17a9be9dfeda2c1
Fabio Massaioli <fabio.massaioli@gmail.com>
2025-03-24 01:08:16 +0100

    Update templates
    Co-authored-by: Ian VanSchooten <ian.vanschooten@gmail.com>

--- v3/internal/templates/sveltekit/frontend/vite.config.js
+++ v3/internal/templates/sveltekit/frontend/vite.config.js
@@ -1,16 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
+import wails from "@wailsio/runtime/plugins/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        fs: {
-          allow: [
-            // search up for workspace root
-            searchForWorkspaceRoot(process.cwd()),
-            // your custom rules
-            './bindings/*',
-          ],
-        },
+  plugins: [sveltekit(), wails("./bindings")],
+  server: {
+    fs: {
+      allow: [
+        // search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+        // your custom rules
+        "./bindings/*",
+      ],
     },
-	plugins: [sveltekit()]
+  },
 });
