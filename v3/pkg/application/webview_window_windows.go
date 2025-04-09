687b79f1752b15aeeb00f9741b08004718898a34
Lea Anthony <lea.anthony@gmail.com>
2025-04-09 21:50:48 +1000

    Fix focus calls when window is disabled or minimised. Fixes #3759 and #4168

--- v3/pkg/application/webview_window_windows.go
+++ v3/pkg/application/webview_window_windows.go
@@ -785,6 +785,14 @@ func (w *windowsWebviewWindow) setFullscreenButtonEnabled(_ bool) {
 
 func (w *windowsWebviewWindow) focus() {
 	w32.SetForegroundWindow(w.hwnd)
+
+	if w.isDisabled() {
+		return
+	}
+	if w.isMinimised() {
+		w.unminimise()
+	}
+
 	w.focusingChromium = true
 	w.chromium.Focus()
 	w.focusingChromium = false
@@ -1036,6 +1044,11 @@ func (w *windowsWebviewWindow) disableIcon() {
 				w32.SWP_NOSIZE|
 				w32.SWP_NOZORDER),
 	)
+}
+
+func (w *windowsWebviewWindow) isDisabled() bool {
+	style := uint32(w32.GetWindowLong(w.hwnd, w32.GWL_STYLE))
+	return style&w32.WS_DISABLED != 0
 }
 
 func (w *windowsWebviewWindow) updateTheme(isDarkMode bool) {
