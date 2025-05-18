b7c6c82cf361e40e0c09fc276bf823404c6e9c5c
Atterpac <michael.capretta@beamtransfer.io>
2025-05-18 16:33:56 -0400

    Add `ApplicationLaunchedWithUrl` event

--- v3/pkg/application/context_application_event.go
+++ v3/pkg/application/context_application_event.go
@@ -1,17 +1,22 @@
 package application
 
+import "log"
+
 var blankApplicationEventContext = &ApplicationEventContext{}
 
 const (
 	openedFiles = "openedFiles"
 	filename    = "filename"
+	url         = "url"
 )
 
+// ApplicationEventContext is the context of an application event
 type ApplicationEventContext struct {
 	// contains filtered or unexported fields
 	data map[string]any
 }
 
+// OpenedFiles returns the opened files from the event context if it was set
 func (c ApplicationEventContext) OpenedFiles() []string {
 	files, ok := c.data[openedFiles]
 	if !ok {
@@ -44,22 +49,29 @@ func (c ApplicationEventContext) getBool(key string) bool {
 	return result
 }
 
+// IsDarkMode returns true if the event context has a dark mode
 func (c ApplicationEventContext) IsDarkMode() bool {
 	return c.getBool("isDarkMode")
 }
 
+// HasVisibleWindows returns true if the event context has a visible window
 func (c ApplicationEventContext) HasVisibleWindows() bool {
 	return c.getBool("hasVisibleWindows")
 }
 
-func (c ApplicationEventContext) setData(data map[string]any) {
+func (c *ApplicationEventContext) setData(data map[string]any) {
 	c.data = data
 }
 
-func (c ApplicationEventContext) setOpenedWithFile(filepath string) {
+func (c *ApplicationEventContext) setOpenedWithFile(filepath string) {
 	c.data[filename] = filepath
 }
 
+func (c *ApplicationEventContext) setURL(openedWithURL string) {
+	c.data[url] = openedWithURL
+}
+
+// Filename returns the filename from the event context if it was set
 func (c ApplicationEventContext) Filename() string {
 	filename, ok := c.data[filename]
 	if !ok {
@@ -67,6 +79,21 @@ func (c ApplicationEventContext) Filename() string {
 	}
 	result, ok := filename.(string)
 	if !ok {
+		return ""
+	}
+	return result
+}
+
+// URL returns the URL from the event context if it was set
+func (c ApplicationEventContext) URL() string {
+	url, ok := c.data[url]
+	if !ok {
+		log.Println("URL not found in event context")
+		return ""
+	}
+	result, ok := url.(string)
+	if !ok {
+		log.Println("URL not a string in event context")
 		return ""
 	}
 	return result
