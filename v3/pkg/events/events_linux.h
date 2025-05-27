593a6e168fd7d16afb61250ba97be2c7f5320c0f
Atterpac <michael.capretta@beamtransfer.io>
2025-05-27 12:07:53 -0400

    new events

--- v3/pkg/events/events_linux.h
+++ v3/pkg/events/events_linux.h
@@ -6,16 +6,16 @@
 extern void processApplicationEvent(unsigned int, void* data);
 extern void processWindowEvent(unsigned int, unsigned int);
 
-#define EventApplicationStartup 1048
-#define EventSystemThemeChanged 1049
-#define EventWindowDeleteEvent 1050
-#define EventWindowDidMove 1051
-#define EventWindowDidResize 1052
-#define EventWindowFocusIn 1053
-#define EventWindowFocusOut 1054
-#define EventWindowLoadChanged 1055
+#define EventApplicationStartup 1049
+#define EventSystemThemeChanged 1050
+#define EventWindowDeleteEvent 1051
+#define EventWindowDidMove 1052
+#define EventWindowDidResize 1053
+#define EventWindowFocusIn 1054
+#define EventWindowFocusOut 1055
+#define EventWindowLoadChanged 1056
 
-#define MAX_EVENTS 1056
+#define MAX_EVENTS 1057
 
 
 #endif
