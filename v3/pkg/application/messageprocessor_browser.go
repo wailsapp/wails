ce29950ba461980982933063613a65e652fd79df
Lea Anthony <lea.anthony@gmail.com>
2025-08-12 20:35:30 +1000

    fix compile issues

--- v3/pkg/application/messageprocessor_browser.go
+++ v3/pkg/application/messageprocessor_browser.go
@@ -30,14 +30,14 @@ func (m *MessageProcessor) processBrowserMethod(method int, rw http.ResponseWrit
 			m.httpError(rw, "Invalid browser call:", errors.New("missing argument 'url'"))
 			return
 		}
-		
+
 		sanitizedURL, err := ValidateAndSanitizeURL(*url)
 		if err != nil {
 			m.Error("OpenURL: invalid URL - %s", err.Error())
-			m.httpError(rw, "Invalid URL: %s", err.Error())
+			m.httpError(rw, fmt.Sprintf("Invalid URL: %s", err.Error()), err)
 			return
 		}
-		
+
 		err = browser.OpenURL(sanitizedURL)
 		if err != nil {
 			m.httpError(rw, "OpenURL failed:", err)
@@ -45,7 +45,7 @@ func (m *MessageProcessor) processBrowserMethod(method int, rw http.ResponseWrit
 		}
 
 		m.ok(rw)
-		m.Info("Runtime Call:", "method", "Browser."+browserMethods[method], "url", sanitizedURL)
+		m.Info("Runtime call:", "method", "Browser."+browserMethods[method], "url", sanitizedURL)
 	default:
 		m.httpError(rw, "Invalid browser call:", fmt.Errorf("unknown method: %d", method))
 		return
