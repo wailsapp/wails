935c04d563a666b0f136ab1679fba76efb93a7ed
Travis McLane <tmclane@gmail.com>
2025-11-10 16:52:43 -0600

    allow unit tests to be Debug=true

--- v3/internal/buildinfo/buildinfo.go
+++ v3/internal/buildinfo/buildinfo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime/debug"
 	"slices"
+	"testing"
 
 	"github.com/samber/lo"
 )
@@ -21,18 +22,19 @@ func Get() (*Info, error) {
 
 	// BuildInfo contains the build info for the application
 	var BuildInfo *debug.BuildInfo
-
 	var ok bool
 	BuildInfo, ok = debug.ReadBuildInfo()
 	if !ok {
 		return nil, fmt.Errorf("could not read build info from binary")
 	}
+
 	result.BuildSettings = lo.Associate(BuildInfo.Settings, func(setting debug.BuildSetting) (string, string) {
 		return setting.Key, setting.Value
 	})
 	result.Version = BuildInfo.Main.Version
+
 	result.Development = -1 != slices.IndexFunc(BuildInfo.Settings, func(setting debug.BuildSetting) bool {
-		return setting.Key == "vcs" && setting.Value == "git"
+		return testing.Testing() || setting.Key == "vcs" && setting.Value == "git"
 	})
 
 	return &result, nil
