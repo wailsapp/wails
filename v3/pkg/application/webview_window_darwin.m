68109fbab3969ba63eaca4976771ea882ab3a847
Dzmitryi Hilimovich <dmitry.gilimovich@gmail.com>
2025-12-27 19:06:09 +0100

    Clean up debug output in webView URL scheme handler
    Removed debug print statements for CSS requests in web view.

--- v3/pkg/application/webview_window_darwin.m
+++ v3/pkg/application/webview_window_darwin.m
@@ -373,14 +373,8 @@ typedef NS_ENUM(NSInteger, MacLiquidGlassStyle) {
 }
 - (void)webView:(nonnull WKWebView *)webView startURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask {
     NSURL *url = urlSchemeTask.request.URL;
-    printf("🎨🎨🎨 [DARWIN] URL SCHEME HANDLER: %s\n", [url.absoluteString UTF8String]);
     fflush(stdout);
     if ([url.path hasSuffix:@".css"] || [url.path containsString:@"style"]) {
-        printf("🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨\n");
-        printf("🎨 [DARWIN] CSS REQUEST INTERCEPTED!\n");
-        printf("🎨 URL: %s\n", [url.absoluteString UTF8String]);
-        printf("🎨 Path: %s\n", [url.path UTF8String]);
-        printf("🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨🎨\n");
         fflush(stdout);
     }
     processURLRequest(self.windowId, urlSchemeTask);
