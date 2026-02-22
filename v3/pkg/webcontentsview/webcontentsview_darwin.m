40b6f8b2dca5b75eb8f85d658a69a9ff9c266db2
Pandelis Zembashis <p@ndel.is>
2026-02-22 03:35:04 +0000

    feat: working

--- v3/pkg/webcontentsview/webcontentsview_darwin.m
+++ v3/pkg/webcontentsview/webcontentsview_darwin.m
@@ -6,78 +6,75 @@ void* createWebContentsView(int x, int y, int w, int h, WebContentsViewPreferenc
     
     WKPreferences *preferences = [[WKPreferences alloc] init];
     
-    // Apply preferences
-    if (@available(macOS 10.11, *)) {
-        [preferences setValue:@(prefs.devTools) forKey:@"developerExtrasEnabled"];
-    }
-    
-    // JavaScript
-    if (@available(macOS 11.0, *)) {
-        WKWebpagePreferences *webpagePreferences = [[WKWebpagePreferences alloc] init];
-        webpagePreferences.allowsContentJavaScript = prefs.javascript;
-        config.defaultWebpagePreferences = webpagePreferences;
-    } else {
-        preferences.javaScriptEnabled = prefs.javascript;
-    }
-    
-    // WebSecurity / CORS
-    if (!prefs.webSecurity) {
-        [preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
-        [preferences setValue:@YES forKey:@"allowUniversalAccessFromFileURLs"];
-    }
-    
-    // Images
-    [preferences setValue:@(prefs.images) forKey:@"loadsImagesAutomatically"];
-    
-    // Plugins
-    [preferences setValue:@(prefs.plugins) forKey:@"plugInsEnabled"];
-    
-    // Fonts
-    if (prefs.defaultFontSize > 0) {
-        [preferences setValue:@(prefs.defaultFontSize) forKey:@"defaultFontSize"];
-    }
-    if (prefs.minimumFontSize > 0) {
-        preferences.minimumFontSize = prefs.minimumFontSize;
-    }
-    
-    config.preferences = preferences;
+    @try {
+        if (@available(macOS 10.11, *)) {
+            [preferences setValue:@(prefs.devTools) forKey:@"developerExtrasEnabled"];
+        }
+        
+        if (@available(macOS 11.0, *)) {
+            WKWebpagePreferences *webpagePreferences = [[WKWebpagePreferences alloc] init];
+            webpagePreferences.allowsContentJavaScript = prefs.javascript;
+            config.defaultWebpagePreferences = webpagePreferences;
+        } else {
+            preferences.javaScriptEnabled = prefs.javascript;
+        }
+        
+        if (!prefs.webSecurity) {
+            @try {
+                [config.preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
+                [config.preferences setValue:@YES forKey:@"allowUniversalAccessFromFileURLs"];
+            } @catch (NSException *e) {}
+        }
+        config.preferences = preferences;
+    } @catch (NSException *e) {}
 
     WKWebView* webView = [[WKWebView alloc] initWithFrame:frame configuration:config];
-    
-    // Zoom factor
-    if (prefs.zoomFactor != 1.0 && prefs.zoomFactor > 0) {
-        if (@available(macOS 11.0, *)) {
-            [webView setPageZoom:prefs.zoomFactor];
-        } else {
-            [webView setMagnification:prefs.zoomFactor];
-        }
-    }
+    [webView setValue:@(NO) forKey:@"drawsTransparentBackground"];
     
     return webView;
 }
 
 void webContentsViewSetBounds(void* view, int x, int y, int w, int h) {
     WKWebView* webView = (WKWebView*)view;
-    [webView setFrame:NSMakeRect(x, y, w, h)];
+    NSView* superview = [webView superview];
+    
+    if (superview != nil) {
+        // macOS standard coordinates: 0,0 is bottom-left
+        CGFloat superHeight = superview.bounds.size.height;
+        CGFloat cocoaY = superHeight - y - h;
+        [webView setFrame:NSMakeRect(x, cocoaY, w, h)];
+    } else {
+        [webView setFrame:NSMakeRect(x, y, w, h)];
+    }
 }
 
 void webContentsViewSetURL(void* view, const char* url) {
     WKWebView* webView = (WKWebView*)view;
     NSString* nsURL = [NSString stringWithUTF8String:url];
     NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:nsURL]];
-    [webView loadRequest:request];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [webView loadRequest:request];
+    });
 }
 
 void webContentsViewExecJS(void* view, const char* js) {
     WKWebView* webView = (WKWebView*)view;
     NSString* script = [NSString stringWithUTF8String:js];
-    [webView evaluateJavaScript:script completionHandler:nil];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [webView evaluateJavaScript:script completionHandler:nil];
+    });
 }
 
 void windowAddWebContentsView(void* nsWindow, void* view) {
     NSWindow* window = (NSWindow*)nsWindow;
     WKWebView* webView = (WKWebView*)view;
-    [window.contentView addSubview:webView positioned:NSWindowAbove relativeTo:nil];
+    
+    // Add directly to the window's root view so we avoid clipping issues
+    [window.contentView addSubview:webView];
+    
+    // Force to front by giving it a high zPosition
+    [webView setWantsLayer:YES];
+    webView.layer.zPosition = 9999.0;
 }
 
 void windowRemoveWebContentsView(void* nsWindow, void* view) {
